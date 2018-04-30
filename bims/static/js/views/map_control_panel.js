696b4078c445cb1d8c38817721e999384d8f690d
Dimas <dimas.ciputra@gmail.com>
2018-04-30 21:13:54 +0700

    Show search panel

--- bims/static/js/views/map_control_panel.js
+++ bims/static/js/views/map_control_panel.js
@@ -1,25 +1,33 @@
 define(['backbone', 'underscore'], function (Backbone, _) {
    return Backbone.View.extend({
        template: _.template($('#map-control-panel').html()),
+       searchBox: null,
        events: {
            'click .search-control': 'searchClicked',
            'click .filter-control': 'filterClicked',
            'click .location-control': 'locationClicked',
+           'click .map-search-close': 'searchCloseClicked',
        },
        initialize: function (options) {
             _.bindAll(this, 'render');
             this.parent = options.parent;
-            this.render();
        },
        searchClicked: function (e) {
+            // show search div
+            this.searchBox.show();
+       },
+       searchCloseClicked: function (e) {
+            this.searchBox.hide();
        },
        filterClicked: function (e) {
        },
        locationClicked: function (e) {
        },
        render: function () {
-           this.$el.html(this.template());
-           return this;
+            this.$el.html(this.template());
+            this.searchBox = this.$el.find('.map-search-box');
+            this.searchBox.hide();
+            return this;
        }
    })
 });
