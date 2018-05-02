1309c3cdd01b64ba7e501e9335ec7a7360b40cd5
Dimas <dimas.ciputra@gmail.com>
2018-05-02 10:21:55 +0700

    Show search panel on click

--- bims/static/js/views/map_control_panel.js
+++ bims/static/js/views/map_control_panel.js
@@ -1,33 +1,55 @@
 define(['backbone', 'underscore'], function (Backbone, _) {
-   return Backbone.View.extend({
-       template: _.template($('#map-control-panel').html()),
-       searchBox: null,
-       events: {
-           'click .search-control': 'searchClicked',
-           'click .filter-control': 'filterClicked',
-           'click .location-control': 'locationClicked',
-           'click .map-search-close': 'searchCloseClicked',
-       },
-       initialize: function (options) {
+    return Backbone.View.extend({
+        template: _.template($('#map-control-panel').html()),
+        searchBox: null,
+        searchBoxOpen: false,
+        events: {
+            'click .search-control': 'searchClicked',
+            'click .filter-control': 'filterClicked',
+            'click .location-control': 'locationClicked',
+            'click .map-search-close': 'searchCloseClicked',
+            'keypress #search': 'searchEnter'
+        },
+        initialize: function (options) {
             _.bindAll(this, 'render');
             this.parent = options.parent;
-       },
-       searchClicked: function (e) {
+        },
+        searchEnter: function (e) {
+            if(e.which === 13) {
+                let searchValue = $(e.target).val();
+                if(searchValue.length < 3) {
+                    console.log('Minimal 3 characters');
+                    return false;
+                }
+                // Do search
+                console.log(searchValue);
+            }
+        },
+        searchClicked: function (e) {
             // show search div
-            this.searchBox.show();
-       },
-       searchCloseClicked: function (e) {
+            if(!this.searchBoxOpen) {
+                this.$el.find('.search-control').addClass('control-panel-selected');
+                this.searchBox.show();
+                this.$el.find('#search').focus();
+                this.searchBoxOpen = true;
+            } else {
+                this.$el.find('.search-control').removeClass('control-panel-selected');
+                this.searchBox.hide();
+                this.searchBoxOpen = false;
+            }
+        },
+        searchCloseClicked: function (e) {
             this.searchBox.hide();
-       },
-       filterClicked: function (e) {
-       },
-       locationClicked: function (e) {
-       },
-       render: function () {
+        },
+        filterClicked: function (e) {
+        },
+        locationClicked: function (e) {
+        },
+        render: function () {
             this.$el.html(this.template());
             this.searchBox = this.$el.find('.map-search-box');
             this.searchBox.hide();
             return this;
-       }
-   })
+        }
+    })
 });
