c7fd3baa0ff466fc88569931dc02429a2e0407ec
ann26 <anita@kartoza.com>
2018-06-07 20:28:15 +0700

    fix serializer

--- bims/api_views/search.py
+++ bims/api_views/search.py
@@ -2,27 +2,25 @@
 __author__ = 'Anita Hapsari <anita@kartoza.com>'
 __date__ = '07/06/18'
 
-from django.utils.translation import ugettext_lazy as _
-from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from bims.documents import BiologicalCollectionRecordDocument
+from bims.serializers.bio_collection_record_doc_serializer import \
+    BiologicalCollectionRecordDocSerializer
 
 
 class SearchObjects(APIView):
     """API for searching using elasticsearch."""
 
     def get(self, request, original_species_name):
-        s = BiologicalCollectionRecordDocument.search().query(
+        results = BiologicalCollectionRecordDocument.search().query(
             "match", original_species_name=original_species_name)
 
         data = {}
-        if s.count() > 0:
-            for hit in s:
-                data['original_species_name'] = hit.original_species_name
-                data['collection_date'] = hit.collection_date
-                data['collector'] = hit.collector
+        if results.count() > 0:
+            serializer = \
+                BiologicalCollectionRecordDocSerializer(results, many=True)
+            return Response(serializer.data)
         else:
             data['results'] = 'Not Found'
-
-        return Response(data)
+            return Response(data)
