1778d619c82972e60f7b94083194c8388c4d7e48
sonlinux <alison.mukoma@yahoo.com>
2018-06-14 14:25:33 +0200

    update requirements in setup

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
             'django-filter==1.1.0',
             'coreapi==2.3.3',
             'pygbif==0.2.0',
+            'django-modelsdoc==0.1.9',
         ],
         dependency_links=[
             'git+https://github.com/soynatan/django-easy-audit.git',
