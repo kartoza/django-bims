63465f8f28646bc57c6e8ee416bd1afcc566e25a
sonlinux <alison.mukoma@yahoo.com>
2018-06-25 10:22:13 +0200

    change to potrait

--- reports/views/stats.py
+++ reports/views/stats.py
@@ -6,26 +6,26 @@ import os
 from django.http import HttpResponse
 
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.pagesizes import A4, letter
 from bims.models.taxon import Taxon
 from bims.models.biological_collection_record import BiologicalCollectionRecord
+
+
 def create_pdf(pathname, current_site):
 
     # initialising the PDF for drawing content on it
-    page = canvas.Canvas(pathname, pagesize=landscape(A4))
+    page = canvas.Canvas(pathname, pagesize=letter)
     width, height = A4
     center = height * 0.5
 
     margin_left = 125
     margin_bottom = 220
 
-    records = Taxon.objects.all()
-    total_collection = records.count()
+    taxa_records = Taxon.objects.all()
+    species_total = taxa_records.count()
 
     biological_total = BiologicalCollectionRecord.objects.count()
-    for record in records:
-        name = record.common_name
-        status = record.iucn_status
+
 
     # we start to draw staff on the PDF
     page.setFillColorRGB(0.1, 0.1, 0.1)
@@ -34,43 +34,50 @@ def create_pdf(pathname, current_site):
     logo = 'reports/static/img/logo.png'
     wildlife_logo = 'reports/static/img/collection/wildlife.png'
     biodiversity_logo = 'reports/static/img/collection/biodiversity.png'
-    page.setFont('Times-Bold', 26)
+    page.setFont('Times-Bold', 16)
     page.drawImage(
-            logo, 370, 480, width = 100, height = 100,
+            logo, 230, 650, width = 100, height = 100,
             preserveAspectRatio = True, mask = 'auto')
-    page.drawCentredString(center, 450, 'Ledet Report')
+    page.drawString(235, 630, 'Ledet Report')
 
-    page.setFont('Times-Roman', 16)
-    page.setFont('Times-Roman', 16)
-    page.drawCentredString(
-		    center, 290, 'Species')
+    page.setFillColorRGB(0.1, 0.1, 0.1)
+    page.setFont('Times-Roman', 12)
+    page.drawString(
+            380, 600, 'Species Name')
+
+    page.setFont('Times-Roman', 10)
+    page.drawString(
+            380, 580, 'Some fancy name')
+
+    page.setFillColorRGB(0.1, 0.1, 0.1)
+    page.setFont('Times-Roman', 12)
+    page.drawString(
+            520, 600, 'ICUN Status')
+
+    page.setFont('Times-Roman', 10)
+    page.drawString(
+            520, 580, 'Available')
+
+
 
     page.drawImage(
-            wildlife_logo, 120, 220, width = 50, height = 50,
+            wildlife_logo, 90, 550, width = 30, height = 30,
             preserveAspectRatio = True, mask = 'auto')
-    page.drawString(180,
-                    230,
-                    'Total Taxa collection: {0}'.format(total_collection))
+    page.setFont('Times-Roman', 10)
+    page.drawString(130,
+                    560,
+                    'Total Species collection: {0}'.format(species_total))
+
 
     page.drawImage(
-		    biodiversity_logo, 120, 150, width = 50, height = 50,
+		    biodiversity_logo, 90, 500, width = 30, height = 30,
 		    preserveAspectRatio = True, mask = 'auto')
-    page.drawString(180,
-                    160,
+    page.setFont('Times-Roman', 10)
+    page.drawString(130,
+                    510,
                     'Total Biodiversity records: {0}'.format(biological_total))
 
 
-
-
-    # footer information.
-    # page.setFont('Times-Roman', 14)
-    # page.drawString(
-    #     margin_left,
-    #     margin_bottom - 10,
-    #     'We are still adding staff to this report.')
-    # page.setFont('Times-Roman', 8)
-
-    # closing the PDF with content
     page.showPage()
     page.save()
 
