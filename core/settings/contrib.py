86232136fca6fd4229f3c6fe772d6e3853f50002
sonlinux <alison.mukoma@yahoo.com>
2018-06-26 09:17:43 +0200

    add species status in pdf

--- core/settings/contrib.py
+++ core/settings/contrib.py
@@ -90,8 +90,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
-# BROKER_URL = 'amqp://guest:guest@%s:5672//' % os.environ['RABBITMQ_HOST']
-BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+BROKER_URL = 'amqp://guest:guest@%s:5672//' % os.environ['RABBITMQ_HOST']
 
 # django modelsdoc settings
 MODELSDOC_APPS = ('bims',)
--- reports/views/stats.py
+++ reports/views/stats.py
@@ -2,11 +2,13 @@ __author__ = 'Alison Mukoma <alison@kartoza.com'
 __copywrite__ = 'kartoza.com'
 
 import os
+from time import gmtime, strftime
 
 from django.http import HttpResponse
 
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, letter
+from reportlab.lib.pagesizes import letter
+
 from bims.models.taxon import Taxon
 from bims.models.biological_collection_record import BiologicalCollectionRecord
 
@@ -15,11 +17,6 @@ def create_pdf(pathname, current_site):
 
     # initialising the PDF for drawing content on it
     page = canvas.Canvas(pathname, pagesize=letter)
-    width, height = A4
-    center = height * 0.5
-
-    margin_left = 125
-    margin_bottom = 220
 
     species_records = Taxon.objects.all()
     species_total = species_records.count()
@@ -40,9 +37,6 @@ def create_pdf(pathname, current_site):
             preserveAspectRatio = True, mask = 'auto')
     page.drawString(250, 630, 'Ledet Report')
 
-
-    # name_width = 380
-    # status_width = 520
     name_height = 530
     status_height = 530
 
@@ -69,8 +63,6 @@ def create_pdf(pathname, current_site):
         name_height -= 20
         status_height -= 20
 
-
-
     page.drawImage(
             wildlife_logo, 90, 550, width = 30, height = 30,
             preserveAspectRatio = True, mask = 'auto')
@@ -78,27 +70,34 @@ def create_pdf(pathname, current_site):
     page.drawString(130,
                     560,
                     'Total Species collection: {0}'.format(species_total))
-
-
     page.drawImage(
 		    biodiversity_logo, 90, 500, width = 30, height = 30,
 		    preserveAspectRatio = True, mask = 'auto')
     page.setFont('Times-Roman', 10)
     page.drawString(130,
                     510,
-                    'Total Biodiversity records: {0}'.format(biological_total))
+                    'Total Bilogical records: {0}'.format(biological_total))
 
+    page.setFont('Times-Bold', 12)
+    page.drawString(230,
+                    480,
+                    'Protected area management role')
 
+    page.setFont('Times-Bold', 12)
+    page.drawString(200,
+                    450,
+                    'Environmental impact management role')
     page.showPage()
     page.save()
 
 
-def view_pdf(request, **kwargs):
+def view_pdf(request):
 
     current_site = request.META['HTTP_HOST']
+    pdf_name = 'ledet_report_' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-    filename = '{}.{}'.format('ledet_report', 'pdf')
-    storage_folder = ('reports')
+    filename = '{}.{}'.format(pdf_name, 'pdf')
+    storage_folder = 'reports'
     pathname = \
         os.path.join(
             '/home/web/media', '{}/{}'.format(storage_folder, filename))
@@ -107,7 +106,7 @@ def view_pdf(request, **kwargs):
         with open(pathname, 'rb') as pdf:
             response = HttpResponse(pdf.read(), content_type='application/pdf')
             response['Content-Disposition'] = \
-                'filename={}.pdf'.format('ledet_report')
+                'filename={}.pdf'.format(pdf_name)
             return response
     else:
         makepath = '/home/web/media/{}/'.format(storage_folder)
@@ -118,5 +117,5 @@ def view_pdf(request, **kwargs):
         with open(pathname, 'rb') as pdf:
             response = HttpResponse(pdf.read(), content_type='application/pdf')
             response['Content-Disposition'] = \
-                'filename={}.pdf'.format('ledet_report')
+                'filename={}.pdf'.format(pdf_name)
             return response
