bd8a1c447a72f0caed7982422f8abb8a8447fbbb
ann26 <anita@kartoza.com>
2018-07-10 14:19:32 +0700

    fix template tag

--- bims/templatetags/contributions.py
+++ bims/templatetags/contributions.py
@@ -10,16 +10,23 @@ register = template.Library()
 @register.filter
 @stringfilter
 def contribution_date(value):
-    user = User.objects.get(username=value)
-    date = CRUDEvent.objects.filter(user=user).latest('datetime').datetime
+    try:
+        user = User.objects.get(username=value)
+        date = CRUDEvent.objects.filter(user=user).latest('datetime').datetime
+    except (User.DoesNotExist, CRUDEvent.DoesNotExist):
+        date = ''
     return date
 
 
 @register.filter
 @stringfilter
 def summary_contribution(value):
-    user = User.objects.get(username=value)
-    action = CRUDEvent.objects.filter(user=user).latest('datetime')
-    event = action.get_event_type_display()
-    content = action.content_type
+    try:
+        user = User.objects.get(username=value)
+        action = CRUDEvent.objects.filter(user=user).latest('datetime')
+        event = action.get_event_type_display()
+        content = action.content_type
+    except (User.DoesNotExist, CRUDEvent.DoesNotExist):
+        event = ''
+        content = ''
     return '{} {}'.format(event, content)
