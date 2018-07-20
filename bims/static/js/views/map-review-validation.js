05652815f6500760eef52160dc610fb4db3b47e6
ann26 <anita@kartoza.com>
2018-07-20 11:47:10 +0700

    add map

---
+++ bims/static/js/views/map-review-validation.js
@@ -0,0 +1,40 @@
+var map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        target: 'map',
+        view: new ol.View({
+          center: [0, 0],
+          zoom: 2
+        })
+      });
+
+var lastLayer = undefined;
+function zoomToObject(lat, lng) {
+    map.getView().setCenter(ol.proj.transform([lat, lng], 'EPSG:4326', 'EPSG:3857'));
+    map.getView().setZoom(6);
+    if(lastLayer !== undefined){
+        map.removeLayer(lastLayer)
+    }
+
+    var iconFeatures=[];
+
+    var iconFeature = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.transform([lat, lng], 'EPSG:4326',
+      'EPSG:3857')),
+    });
+
+    iconFeatures.push(iconFeature);
+
+    var vectorSource = new ol.source.Vector({
+      features: iconFeatures
+    });
+
+    var vectorLayer = new ol.layer.Vector({
+      source: vectorSource
+    });
+    lastLayer = vectorLayer;
+    map.addLayer(vectorLayer);
+}
