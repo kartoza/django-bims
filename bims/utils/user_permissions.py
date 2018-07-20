08d63edab24be4f207ba6efb7ee46caa38deed70
ann26 <anita@kartoza.com>
2018-07-20 16:43:46 +0700

    update group using category

--- bims/utils/user_permissions.py
+++ bims/utils/user_permissions.py
@@ -8,7 +8,7 @@ class ValidatorRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
         validator = \
             'validator' in request.user.groups.all().values_list(
-                'name', flat=True)
+                'groupprofile__categories__name', flat=True)
 
         if not request.user.is_authenticated():
             return self.handle_no_permission(request)
