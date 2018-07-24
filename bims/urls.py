a51f34ad495d1231b5149e28dce541b81daec6fc
Anita Hapsari <anita@kartoza.com>
2018-07-24 11:58:37 +0700

    fix activate user (#158)
    * fix activate user
    
    * redirect url profile
    
    * add login required
    
    * flake8 fix

--- bims/urls.py
+++ bims/urls.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url, include
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 from rest_framework.documentation import include_docs_urls
-from django.contrib.auth.decorators import login_required
 from bims.views.map import MapPageView
 from bims.views.landing_page import LandingPageView
-from bims.views.user_profile import UserProfileView
 from bims.api_views.location_site import (
     LocationSiteList,
     LocationSiteDetail,
@@ -79,7 +80,11 @@ bibliography_urls = [
 urlpatterns = [
     url(r'^$', LandingPageView.as_view(), name='landing-page'),
     url(r'^map/$', MapPageView.as_view()),
-    url(r'^profile/$', UserProfileView.as_view(), name='user-profile'),
+    url(r'^profile/$',
+        login_required(lambda request: RedirectView.as_view(
+            url=reverse_lazy('profile_detail', kwargs={
+                'username': request.user.username
+            }), permanent=False)(request)), name='user-profile'),
     url(r'^upload/$', login_required(CsvUploadView.as_view()),
         name='csv-upload'),
     url(r'^upload_shp/$', login_required(ShapefileUploadView.as_view()),
