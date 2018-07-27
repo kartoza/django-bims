71196926d7c664b66bbae2aaf20b734626726b99
meomancer <meomancer@gmail.com>
2018-07-27 14:31:57 +0700

    update colum of export file

--- bims/serializers/bio_collection_serializer.py
+++ bims/serializers/bio_collection_serializer.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import (
     GeoFeatureModelSerializer, GeometrySerializerMethodField)
 from bims.models.biological_collection_record import BiologicalCollectionRecord
-from bims.serializers.taxon_serializer import TaxonSerializer
+from bims.serializers.taxon_serializer import (
+    TaxonSerializer,
+    TaxonExportSerializer
+)
 
 
 class BioCollectionSerializer(serializers.ModelSerializer):
@@ -44,67 +47,110 @@ class BioCollectionOneRowSerializer(serializers.ModelSerializer):
     """
     Serializer for biological collection record.
     """
-    owner = serializers.SerializerMethodField()
-    validated = serializers.BooleanField(required=True)
-    x = serializers.SerializerMethodField()
-    y = serializers.SerializerMethodField()
-    site = serializers.SerializerMethodField()
+    location_site = serializers.SerializerMethodField()
+    latitude = serializers.SerializerMethodField()
+    longitude = serializers.SerializerMethodField()
+    species_name = serializers.SerializerMethodField()
+    notes = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    date = serializers.SerializerMethodField()
+    collector = serializers.SerializerMethodField()
 
-    def get_x(self, obj):
-        if obj.site and obj.site.geometry_point:
-            return obj.site.geometry_point.x
-        return ''
-
-    def get_y(self, obj):
-        if obj.site and obj.site.geometry_point:
-            return obj.site.geometry_point.y
-        return ''
-
-    def get_site(self, obj):
+    def get_location_site(self, obj):
         if obj.site:
             return obj.site.name
         return ''
 
-    def get_owner(self, obj):
-        if obj.owner:
-            return '%s,%s' % (obj.owner.pk, obj.owner.username)
+    def get_latitude(self, obj):
+        if obj.site and obj.site.geometry_point:
+            return obj.site.geometry_point.y
+        return ''
+
+    def get_longitude(self, obj):
+        if obj.site and obj.site.geometry_point:
+            return obj.site.geometry_point.x
+        return ''
+
+    def get_species_name(self, obj):
+        return obj.original_species_name
+
+    def get_notes(self, obj):
+        return obj.notes
+
+    def get_category(self, obj):
+        return obj.category
+
+    def get_date(self, obj):
+        if obj.collection_date:
+            return obj.collection_date.strftime('%Y-%m-%d')
+
+    def get_collector(self, obj):
+        return obj.collector
 
     class Meta:
         model = BiologicalCollectionRecord
-        fields = '__all__'
+        fields = [
+            'location_site', 'latitude', 'longitude',
+            'species_name', 'notes', 'category',
+            'date', 'collector']
 
     def to_representation(self, instance):
-        result = super(BioCollectionOneRowSerializer, self).to_representation(
+        result = super(
+            BioCollectionOneRowSerializer, self).to_representation(
             instance)
-        taxonomy = TaxonSerializer(instance.taxon_gbif_id).data
+        taxonomy = TaxonExportSerializer(instance.taxon_gbif_id).data
         result.update(taxonomy)
         return result
 
 
 class BioCollectionGeojsonSerializer(GeoFeatureModelSerializer):
     geometry = GeometrySerializerMethodField()
-    site = serializers.SerializerMethodField()
+    location_site = serializers.SerializerMethodField()
+    species_name = serializers.SerializerMethodField()
+    notes = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    date = serializers.SerializerMethodField()
+    collector = serializers.SerializerMethodField()
+
+    def get_location_site(self, obj):
+        if obj.site:
+            return obj.site.name
+        return ''
+
+    def get_species_name(self, obj):
+        return obj.original_species_name
+
+    def get_notes(self, obj):
+        return obj.notes
+
+    def get_category(self, obj):
+        return obj.category
+
+    def get_date(self, obj):
+        if obj.collection_date:
+            return obj.collection_date.strftime('%Y-%m-%d')
+
+    def get_collector(self, obj):
+        return obj.collector
 
     def get_geometry(self, obj):
         if obj.site:
             return obj.site.get_geometry()
         return None
 
-    def get_site(self, obj):
-        if obj.site:
-            return obj.site.name
-        return ''
-
     class Meta:
         model = BiologicalCollectionRecord
         geo_field = 'geometry'
-        exclude = []
+        fields = [
+            'location_site', 'species_name', 'notes', 'category',
+            'date', 'collector']
 
     def to_representation(self, instance):
-        result = super(BioCollectionGeojsonSerializer, self).to_representation(
+        result = super(
+            BioCollectionGeojsonSerializer, self).to_representation(
             instance)
         try:
-            taxonomy = TaxonSerializer(instance.taxon_gbif_id).data
+            taxonomy = TaxonExportSerializer(instance.taxon_gbif_id).data
             result['properties'].update(taxonomy)
         except KeyError:
             pass
--- bims/serializers/taxon_serializer.py
+++ bims/serializers/taxon_serializer.py
@@ -28,3 +28,31 @@ class TaxonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Taxon
         fields = '__all__'
+
+
+class TaxonExportSerializer(serializers.ModelSerializer):
+    """
+    Serializer for taxon collection model.
+    """
+    iucn_status_sensitive = serializers.SerializerMethodField()
+    iucn_status_name = serializers.SerializerMethodField()
+
+    def get_iucn_status_sensitive(self, obj):
+        if obj.iucn_status:
+            return obj.iucn_status.sensitive
+        else:
+            return None
+
+    def get_iucn_status_name(self, obj):
+        if obj.iucn_status:
+            return obj.iucn_status.category
+        else:
+            return None
+
+    class Meta:
+        model = Taxon
+        fields = [
+            'scientific_name', 'kingdom', 'phylum',
+            'taxon_class', 'order', 'family', 'genus', 'species',
+            'iucn_status_sensitive', 'iucn_status_name'
+        ]
