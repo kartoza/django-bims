a49cf0c1e4a7099bf1c1859bd657e6a5ea335a96
Dimas Ciputra <dimas.ciputra@gmail.com>
2018-08-01 15:28:46 +0700

    Fix validate permissions (#184)

--- bims/api_views/validate_object.py
+++ bims/api_views/validate_object.py
@@ -1,12 +1,21 @@
 # coding=utf-8
 from rest_framework.views import APIView
-from django.http import HttpResponse, JsonResponse
 from rest_framework import status
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
 from bims.models.biological_collection_record import BiologicalCollectionRecord
-from bims.utils.user_permissions import ValidatorRequiredMixin
 
 
-class ValidateObject(ValidatorRequiredMixin, APIView):
+class ValidateObject(UserPassesTestMixin, LoginRequiredMixin, APIView):
+
+    def test_func(self):
+        return self.request.user.has_perm('bims.can_validate_data')
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'You don\'t have permission '
+                                     'to validate collection data')
+        return super(ValidateObject, self).handle_no_permission()
 
     def get(self, request):
         object_pk = request.GET.get('pk', None)
