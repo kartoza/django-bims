308cd6f5a58256751b80729e04b4f619eed5d6f6
Dimas <dimas.ciputra@gmail.com>
2018-08-06 17:23:30 +0700

    Fix celery tasks not recognized

--- bims/celery.py
+++ bims/celery.py
@@ -1,12 +1,11 @@
 from __future__ import absolute_import
 
 from celery import Celery
-from django.conf import settings
 
 app = Celery('bims')
 
 app.config_from_object('django.conf:settings', namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
--- core/settings/celery_settings.py
+++ core/settings/celery_settings.py
@@ -1,11 +1,11 @@
 
-CELERYBEAT_SCHEDULE = {
+CELERY_BEAT_SCHEDULE = {
     'update_search_index': {
-        'task': 'tasks.update_search_index',
+        'task': 'bims.tasks.update_search_index',
         'schedule': 18000.0,  # update every 5 hours
     },
     'update_cluster': {
-        'task': 'tasks.update_cluster',
+        'task': 'bims.tasks.update_cluster',
         'schedule': 18000.0,  # update every 5 hours
     },
 }
