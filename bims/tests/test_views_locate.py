cff88acded239a780a0aa6c4de2b1fca832dd210
Ismail Sunni <imajimatika@gmail.com>
2018-08-09 17:23:02 +0700

    Add utilities function to filter Farm ID.

---
+++ bims/tests/test_views_locate.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""Test Views Locate."""
+
+import os
+from django.test import SimpleTestCase
+
+from bims.views.locate import get_farm_ids, parse_locate_return
+
+test_data_directory = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'data')
+
+
+
+class TestLocateView(SimpleTestCase):
+    """Test Locate View."""
+
+    def test_locate_view(self):
+        farms = get_farm_ids('C0010000000000010000%')
+        for farm_key in farms.keys():
+            self.assertIn('C0010000000000010000', farm_key)
+        self.assertEqual(len(farms), 4)
+
+    def test_parse_locate_returns(self):
+        """Test parsing locate returns xml document."""
+        wfs_document_path = os.path.join(
+            test_data_directory, 'geoserver_farm_id.xml')
+        self.assertTrue(os.path.exists(wfs_document_path))
+        with open(wfs_document_path) as file:
+            wfs_string = file.read()
+
+        features = parse_locate_return(wfs_string)
+
+        self.assertEqual(len(features), 4)
---
+++ bims/views/locate.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+"""Locate utilities view."""
+
+import requests
+from xml.dom import minidom
+from django.contrib.gis.geos import GEOSGeometry
+from django.conf import settings
+
+from django.http import QueryDict
+
+
+def get_farm_ids(farm_id_pattern):
+    """Retrieve Farm IDs from a farm id pattern.
+
+    :param farm_id_pattern: A pattern to farm ID.
+    :type farm_id_pattern: basestring
+
+    :returns: A dictionary of farm ID as the key and its envelope as the value.
+    :rtype: dict
+    """
+    url = settings.FARM_GEOSERVER_URL
+
+    parameters = {
+        'SERVICE': 'WFS',
+        'VERSION': '2.0.0',
+        'REQUEST': 'GETFEATURE',
+        'TYPENAME': settings.FARM_WORKSPACE + ':' + settings.FARM_LAYER_NAME,
+        'PROPERTYNAME': settings.FARM_ID_COLUMN,
+        'CQL_FILTER': "%s LIKE '%s'" % (
+            settings.FARM_ID_COLUMN, farm_id_pattern)
+    }
+
+    query_dict = QueryDict('', mutable=True)
+    query_dict.update(parameters)
+
+    if '?' in url:
+        url = url + '&' + query_dict.urlencode()
+    else:
+        url = url + '?' + query_dict.urlencode()
+
+    request = requests.get(url)
+    content = request.content
+
+    return parse_locate_return(content)
+
+
+def parse_locate_return(xml_document):
+    """Parse locate xml document from requesting GeoServer.
+
+    :param xml_document: XML document from GeoServer returns.
+    :type xml_document: basestring
+
+    :returns: A dictionary of farm ID as the key and its envelope as the value.
+    :rtype: dict
+    """
+    xmldoc = minidom.parseString(xml_document)
+    features = {}
+
+    try:
+        wfs_xml_features = xmldoc.getElementsByTagName('wfs:member')
+        for wfs_xml_feature in wfs_xml_features:
+            id_tag = settings.FARM_WORKSPACE + ':' + settings.FARM_ID_COLUMN
+            farm_id_xml = wfs_xml_feature.getElementsByTagName(id_tag)[0]
+            farm_id = farm_id_xml.childNodes[0].nodeValue
+            bounded_gml = wfs_xml_feature.getElementsByTagName(
+                'gml:boundedBy')[0]
+            bounded_gml_dom = bounded_gml.childNodes[0]
+            envelope = GEOSGeometry.from_gml(bounded_gml_dom.toxml()).extent
+            features[farm_id] = envelope
+        return features
+    except IndexError:
+        return features
