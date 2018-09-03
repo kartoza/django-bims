568d9a197d7822203dcb3344ecef9f0f17568dbb
Dimas Ciputra <dimas.ciputra@gmail.com>
2018-09-03 11:49:33 +0700

    Show site details in panel view (#308)

--- bims/static/js/views/location_site_detail.js
+++ bims/static/js/views/location_site_detail.js
@@ -28,7 +28,55 @@ define(['backbone', 'ol', 'shared'], function (Backbone, ol, Shared) {
         },
         renderSiteDetail: function (data) {
             var $detailWrapper = $('<div></div>');
-            $detailWrapper.append('<div class="side-panel-content">No detail for this site.</div>');
+            var locationContext = {};
+            var maxPanelThatShouldBeOpen = 1;
+
+            if (data.hasOwnProperty('location_context_document_json')) {
+                locationContext = data['location_context_document_json'];
+            }
+            if (locationContext.hasOwnProperty('context_group_values')) {
+                 var contextGroups = locationContext['context_group_values'];
+                 $.each(contextGroups, function (index, value) {
+                     var $classWrapper = $('<div class="sub-species-wrapper"></div>');
+
+                     $classWrapper.click(function (e) {
+                         $(this).find('.result-search').toggle();
+                     });
+
+                     var subPanel = _.template($('#site-detail-sub-title').html());
+                     var siteDetailTemplate = _.template($('#site-detail-registry-values').html());
+                     $classWrapper.append(subPanel({
+                         name: value['name']
+                     }));
+
+                     if (value.hasOwnProperty('service_registry_values')) {
+                         $.each(value['service_registry_values'], function (service_index, service_value) {
+                             if (service_value.hasOwnProperty('name') &&
+                                 service_value.hasOwnProperty('value')) {
+                                 var service_value_name = service_value['name'];
+                                 var service_value_value = service_value['value'];
+
+                                 if (service_value_value &&  service_value_name) {
+                                    $classWrapper.append(
+                                        siteDetailTemplate({
+                                            name: service_value_name,
+                                            value: service_value_value
+                                        })
+                                    );
+                                 }
+                             }
+                         })
+                     }
+
+                     $detailWrapper.append($classWrapper);
+                     if (index > maxPanelThatShouldBeOpen - 1) {
+                         $classWrapper.find('.result-search').hide();
+                     }
+
+                 })
+            } else {
+                $detailWrapper.append('<div class="side-panel-content">No detail for this site.</div>');
+            }
             return $detailWrapper;
         },
         renderDashboardDetail: function (data) {
