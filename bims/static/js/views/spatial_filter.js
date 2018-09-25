1d7083910202d423b6c4c7e3f1d1e14b23e1890f
Dimas <dimas.ciputra@gmail.com>
2018-09-25 12:12:26 +0700

    Add upload functionality in modal

--- bims/static/js/views/spatial_filter.js
+++ bims/static/js/views/spatial_filter.js
@@ -1,12 +1,37 @@
-define(['backbone', 'underscore', 'jquery', 'shared', 'ol'], function (Backbone, _, $, Shared, ol) {
+define([
+    'backbone',
+    'underscore',
+    'jquery',
+    'shared',
+    'ol',
+    'jquery.fileupload',
+    'jquery.fileupload-process',
+    'jquery.fileupload-validate'], function (Backbone, _, $, Shared, ol) {
     return Backbone.View.extend({
         template: _.template($('#spatial-filter-panel').html()),
         events: {
-            'click .close-button': 'close'
+            'click .close-button': 'close',
+            'click #spatial-filter-panel-upload': 'panelUploadClicked',
+            'click .close-upload-boundary-modal': 'closeModal',
+            'click .btn-boundary-upload': 'btnUploadClicked'
         },
         render: function () {
             this.$el.html(this.template());
             this.$el.find('.map-control-panel-box').hide();
+            this.fileupload = this.$el.find('#fileupload');
+
+            this.fileupload.fileupload({
+                acceptFileTypes: /(\.|\/)(shp|shx|dbf)$/i,
+                dataType: 'json',
+                done: function (e, data) {
+                    if (data.result.is_valid) {
+                        $("#file-list tbody").prepend(
+                            "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+                        )
+                    }
+                }
+            });
+
             return this;
         },
         isOpen: function () {
@@ -20,6 +45,19 @@ define(['backbone', 'underscore', 'jquery', 'shared', 'ol'], function (Backbone,
         },
         hide: function () {
             this.$el.find('.map-control-panel-box').hide();
+        },
+        panelUploadClicked: function (e) {
+            // Show upload modal
+            this.$el.find('.modal').show();
+        },
+        closeModal: function (e) {
+            this.$el.find('.modal').hide();
+        },
+        btnUploadClicked: function (e) {
+            this.fileupload.click();
+        },
+        fileUpload: function () {
+
         }
     })
 });
