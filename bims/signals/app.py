18d77ecd2d80bede0e958a7cb3ca7f6d57d3d0c6
Dimas Ciputra <dimas.ciputra@gmail.com>
2018-10-29 16:08:48 +0700

    Disable i18n for admin page (#461)

--- bims/signals/app.py
+++ bims/signals/app.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+
 class SignalsConfig(AppConfig):
     name = 'bims.signals'
     verbose_name = _('signals')
--- bims/urls.py
+++ bims/urls.py
@@ -1,50 +1,20 @@
 # coding=utf-8
 
 from django.contrib.auth.decorators import login_required
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework.documentation import include_docs_urls
 from bims.views.map import MapPageView
 from bims.views.tracking import dashboard
 from bims.views.landing_page import LandingPageView
-from bims.api_views.boundary import (
-    BoundaryList,
-    BoundaryGeojson
-)
-from bims.api_views.location_site import (
-    LocationSiteList,
-    LocationSiteDetail,
-    LocationSiteClusterList
-)
-from bims.api_views.location_type import (
-    LocationTypeAllowedGeometryDetail
-)
-from bims.api_views.non_biodiversity_layer import (
-    NonBiodiversityLayerList
-)
-from bims.api_views.taxon import TaxonDetail
-from bims.api_views.cluster import ClusterList
+
 from bims.api_views.collection import (
-    GetCollectionExtent,
     CollectionDownloader,
-    ClusterCollection
 )
-from bims.api_views.collector import CollectorList
-from bims.api_views.reference_category import ReferenceCategoryList
-from bims.api_views.category_filter import CategoryList
-from bims.api_views.reference_list import ReferenceList
-from bims.api_views.search import SearchObjects
-from bims.api_views.validate_object import ValidateObject
-from bims.api_views.get_biorecord import (
-    GetBioRecordDetail,
-    GetBioRecords
-)
+
 from bims.api_views.get_feature_info import GetFeatureInfo
-from bims.api_views.non_validated_record import GetNonValidatedRecords
-from bims.api_views.hide_popup_info_user import HidePopupInfoUser
 from bims.views.links import LinksCategoryView
 from bims.views.activate_user import activate_user
 from bims.views.csv_upload import CsvUploadView
@@ -58,67 +28,7 @@ from bims.views.non_validated_list import NonValidatedObjectsView
 from bims.views.non_validated_user_list import NonValidatedObjectsUserView
 from bims.views.bio_records_edit import BioRecordsUpdateView
 from bims.views.collection_upload import CollectionUploadView
-from bims.api_views.send_notification_to_validator import \
-    SendNotificationValidation
-from bims.views.locate import filter_farm_ids_view, get_farm_view
-from bims.api_views.user_boundary import UserBoundaryList
 
-api_urls = [
-    url(r'^api/location-type/(?P<pk>[0-9]+)/allowed-geometry/$',
-        LocationTypeAllowedGeometryDetail.as_view()),
-    url(r'^api/location-site/cluster/$', LocationSiteClusterList.as_view()),
-    url(r'^api/location-site/$', LocationSiteList.as_view()),
-    url(r'^api/location-site-detail/$',
-        LocationSiteDetail.as_view(),
-        name='location-site-detail'),
-    url(r'^api/taxon/(?P<pk>[0-9]+)/$',
-        TaxonDetail.as_view()),
-    url(r'^api/cluster/(?P<administrative_level>\w+)/$',
-        ClusterList.as_view()),
-    url(r'^api/collection/extent/$',
-        GetCollectionExtent.as_view()),
-    url(r'^api/collection/cluster/$',
-        ClusterCollection.as_view()),
-    url(r'^api/collection/download/$',
-        CollectionDownloader.as_view()),
-    url(r'^collection/check_process/$',
-        CollectionDownloader.as_view()),
-    url(r'^api/search/$',
-        SearchObjects.as_view(), name='search-api'),
-    url(r'^api/boundary/geojson$',
-        BoundaryGeojson.as_view(), name='boundary-geojson'),
-    url(r'^api/list-boundary/$',
-        BoundaryList.as_view(), name='list-boundary'),
-    url(r'^api/list-user-boundary/$',
-        UserBoundaryList.as_view(), name='list-user-boundary'),
-    url(r'^api/list-collector/$',
-        CollectorList.as_view(), name='list-collector'),
-    url(r'^api/list-category/$',
-        CategoryList.as_view(), name='list-date-category'),
-    url(r'^api/list-reference/$',
-        ReferenceList.as_view(), name='list-reference'),
-    url(r'^api/list-non-biodiversity/$',
-        NonBiodiversityLayerList.as_view(),
-        name='list-non-biodiversity-layer'),
-    url(r'^api/validate-object/$',
-        ValidateObject.as_view(), name='validate-object'),
-    url(r'^api/get-bio-object/$',
-        GetBioRecordDetail.as_view(), name='get-bio-object'),
-    url(r'^api/get-bio-records/$',
-        GetBioRecords.as_view(), name='get-bio-records'),
-    url(r'^api/get-unvalidated-records/$',
-        GetNonValidatedRecords.as_view(), name='get-unvalidated-records'),
-    url(r'^api/send-email-validation/$',
-        SendNotificationValidation.as_view(), name='send-email-validation'),
-    url(r'^api/filter-farm-id/$',
-        filter_farm_ids_view, name='filter-farm-id'),
-    url(r'^api/get-farm/(?P<farm_id>[\w-]+)/$',
-        get_farm_view, name='get-farm'),
-    url(r'api/hide-popup-info/$',
-        HidePopupInfoUser.as_view(), name='hide-popup-user'),
-    url(r'^api/list-reference-category/$',
-        ReferenceCategoryList.as_view(), name='list-reference-category'),
-]
 
 
 urlpatterns = [
@@ -139,7 +49,6 @@ urlpatterns = [
         process_user_boundary_shapefiles,
         name='process_user_boundary_shapefiles'),
     url(r'^links/$', LinksCategoryView.as_view(), name = 'link_list'),
-    url(r'^api/docs/', include_docs_urls(title='BIMS API')),
     url(r'^activate-user/(?P<username>[\w-]+)/$',
         activate_user, name='activate-user'),
     url(r'^under-development/$',
@@ -156,5 +65,13 @@ urlpatterns = [
         name='upload-collection'),
     url(r'^get_feature/$',
         csrf_exempt(GetFeatureInfo.as_view()),
-        name='get-feature')
-] + api_urls
+        name='get-feature'),
+    url(r'^collection/check_process/$',
+        CollectionDownloader.as_view()),
+]
+
+# Api urls
+urlpatterns += [  # '',
+    url(r'^api/',
+        include('bims.api_urls')),
+]
