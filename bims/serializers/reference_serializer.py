bc96098c626afed0bb084933c823be3c2caba3a1
dimasciput <dimas.ciputra@gmail.com>
2018-11-14 16:57:03 +0700

    Show references category in capture forms

--- bims/serializers/reference_serializer.py
+++ bims/serializers/reference_serializer.py
@@ -6,6 +6,15 @@ class ReferenceSerializer(serializers.ModelSerializer):
     """
     Serializer for cluster model.
     """
+    journal = serializers.SerializerMethodField()
+    journal_abbreviation = serializers.SerializerMethodField()
+
+    def get_journal(self, obj):
+        return obj.journal.name
+
+    def get_journal_abbreviation(self, obj):
+        return obj.journal.abbreviation
+
     class Meta:
         model = Entry
         fields = '__all__'
