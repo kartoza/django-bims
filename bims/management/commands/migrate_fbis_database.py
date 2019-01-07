ca1704150cc3e570fc6ae0c803ffe2b8c25ea15d
dimasciput <dimas.ciputra@gmail.com>
2019-01-07 10:42:18 +0700

    Update command to migrate fbis user

--- bims/management/commands/migrate_fbis_database.py
+++ bims/management/commands/migrate_fbis_database.py
@@ -36,41 +36,51 @@ class Command(BaseCommand):
             )
             username_value = dhead['UserName'][i].replace(' ', '_').lower()
             date_joined = dateutil.parser.parse(dhead['DateFrom'][i])
+            email_value = dhead['Email'][i]
+            if Profile.objects.filter(username=username_value).exists():
+                profiles = Profile.objects.filter(username=username_value)
+                if not profiles.filter(email=email_value).exists():
+                    same_username = len(profiles)
+                    username_value += '_%s' % str(same_username)
             profile, created = Profile.objects.get_or_create(
                 username=username_value,
-                first_name=dhead['FirstName'][i],
-                last_name=dhead['Surname'][i],
                 email=dhead['Email'][i],
-                date_joined=date_joined,
-                fax=str(dhead['FaxNumber'][i]),
-                delivery=dhead['PostalAddress'][i],
-                zipcode=dhead['PostalCode'][i],
-                position=dhead['Qualifications'][i],
-                voice=str(dhead['Telephone'][i]),
-                password=password_value,
                 is_active=True,
             )
+
+            profile.first_name = dhead['FirstName'][i]
+            profile.last_name = dhead['Surname'][i]
+            profile.date_joined = date_joined
+            profile.fax = str(dhead['FaxNumber'][i])
+            profile.delivery = dhead['PostalAddress'][i]
+            profile.zipcode = dhead['PostalCode'][i]
+            profile.position = dhead['Qualifications'][i]
+            profile.voice = str(dhead['Telephone'][i])
+            profile.password = password_value
+            profile.save()
 
             # Other information
             bims_profile, bims_created = BimsProfile.objects.get_or_create(
                 user=profile,
-                qualifications=dhead['Qualifications'][i],
-                fbis_username=dhead['UserName'][i],
-                other=dhead['Other'][i],
-                data={
-                    'PasswordHint': str(dhead['PasswordHint'][i]),
-                    'RegionPolID': str(dhead['RegionPolID'][i]),
-                    'OrganisationID': str(dhead['OrganisationID'][i]),
-                    'RegionalChampion': str(dhead['RegionalChampion'][i]),
-                    'NTUserName': str(dhead['NTUserName'][i]),
-                    'SASS4': str(dhead['SASS4'][i]),
-                    'RipVegIndex': str(dhead['RipVegIndex'][i]),
-                    'FAIIndex': str(dhead['FAIIndex'][i]),
-                    'DateModified': str(dhead['DateModified'][i])
-                }
             )
 
-            fbis_uuid = FbisUUID.objects.create(
+            bims_profile.qualifications = dhead['Qualifications'][i]
+            bims_profile.fbis_username = dhead['UserName'][i]
+            bims_profile.other = dhead['Other'][i]
+            bims_profile.data = {
+                'PasswordHint': str(dhead['PasswordHint'][i]),
+                'RegionPolID': str(dhead['RegionPolID'][i]),
+                'OrganisationID': str(dhead['OrganisationID'][i]),
+                'RegionalChampion': str(dhead['RegionalChampion'][i]),
+                'NTUserName': str(dhead['NTUserName'][i]),
+                'SASS4': str(dhead['SASS4'][i]),
+                'RipVegIndex': str(dhead['RipVegIndex'][i]),
+                'FAIIndex': str(dhead['FAIIndex'][i]),
+                'DateModified': str(dhead['DateModified'][i])
+            }
+            bims_profile.save()
+
+            fbis_uuid, fbis_created = FbisUUID.objects.get_or_create(
                 content_type=ctype,
                 object_id=profile.id,
                 uuid=dhead['UserID'][i]
