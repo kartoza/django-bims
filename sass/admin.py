bba4c47ece87d2824cb35da1096ba27abff5755f
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-01-18 10:37:48 +0700

    Add SASS 5 models (#691)

--- sass/admin.py
+++ sass/admin.py
@@ -15,6 +15,8 @@ from sass.models import (
     SiteVisitTaxon,
     Chem,
     SiteVisitChem,
+    SASS5Sheet,
+    SASS5Record,
 )
 
 
@@ -141,6 +143,30 @@ class SiteVisitChemAdmin(admin.ModelAdmin):
     )
 
 
+class SASS5SheetAdmin(admin.ModelAdmin):
+    list_display = (
+        'date',
+        'rhp_site_code',
+        'location_site'
+    )
+
+    raw_id_fields = (
+        'location_site',
+    )
+
+
+class SASS5RecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'sass_sheet',
+        'family',
+        'count'
+    )
+
+    raw_id_fields = (
+        'sass_sheet',
+    )
+
+
 # Register your models here.
 admin.site.register(River, RiverAdmin)
 admin.site.register(SiteVisit, SiteVisitAdmin)
@@ -153,3 +179,5 @@ admin.site.register(SassValidationStatus, SassValidationStatusAdmin)
 admin.site.register(SiteVisitTaxon, SiteVisitTaxonAdmin)
 admin.site.register(Chem, ChemAdmin)
 admin.site.register(SiteVisitChem, SiteVisitChemAdmin)
+admin.site.register(SASS5Sheet, SASS5SheetAdmin)
+admin.site.register(SASS5Record, SASS5RecordAdmin)
---
+++ sass/models/sass5_record.py
@@ -0,0 +1,53 @@
+from django.contrib.gis.db import models
+
+
+class SASS5Record(models.Model):
+
+    sass_sheet = models.ForeignKey(
+        'sass.SASS5Sheet',
+        verbose_name='SASS Sheet',
+        help_text='SASS Sheet',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+
+    family = models.ForeignKey(
+        'bims.Taxonomy',
+        verbose_name='Family',
+        help_text='Taxonomy Family',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    count = models.IntegerField(
+        help_text='Count',
+        null=True,
+        blank=True
+    )
+
+    stone_and_rock = models.IntegerField(
+        verbose_name='Stone and Rock (S)',
+        help_text='Stone and Rock (S)',
+        null=True,
+        blank=True
+    )
+
+    vegetation = models.IntegerField(
+        verbose_name='Vegetation (Veg)',
+        help_text='Vegetation (Veg)',
+        null=True,
+        blank=True
+    )
+
+    gravel_sand_mud = models.IntegerField(
+        verbose_name='Gravel Sand Mud (GSM)',
+        help_text='Gravel Sand Mud (GSM)',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'SASS 5 Record'
+        verbose_name_plural = 'SASS 5 Records'
