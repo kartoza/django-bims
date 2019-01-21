da074a37b25c0ea11acdd07eabade6fc5fdf31b5
Dimas <dimas.ciputra@gmail.com>
2019-01-21 18:40:59 +0700

    Update sass view

--- sass/urls.py
+++ sass/urls.py
@@ -5,5 +5,8 @@ from sass.views.sass_list import SassListView
 
 urlpatterns = [
     url(r'^(?P<site_id>\d+)/$', FormView.as_view(), name='sass-form-page'),
+    url(r'^update/(?P<sass_id>\d+)/$',
+        FormView.as_view(),
+        name='sass-update-page'),
     url(r'^list/$', SassListView.as_view(), name='sass-list-page'),
 ]
