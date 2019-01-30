b0a6c1f23081e5a73432fd1d3bf60720f0aa08f7
Christiaanvdm <Christiaan@kartoza.com>
2019-01-30 14:18:15 +0200

    Added column 0, and headers for each column

--- bims/templatetags/jsonify.py
+++ bims/templatetags/jsonify.py
@@ -21,11 +21,11 @@ def get_html_for_radio_group(name, sass_rating):
     try:
         value = int(sass_rating[-1])
     except:
-        value = 1
+        value = 0
     result_html = (
                 '<div class="form-control sass-radio-label" '
                 'name="{name}">').format(name = name)
-    for x in range(0, 5):
+    for x in range(0, 6):
         if x == (value - 1):
             checked = 'checked'
         else:
@@ -39,7 +39,17 @@ def get_html_for_radio_group(name, sass_rating):
                         '{checked}>').format(
                                         name=name,
                                         checked=checked,
-                                        x = x + 1,
+                                        x = x,
                                         starting_space=starting_space)
     result_html += '</div>'
+    return result_html
+
+@register.simple_tag
+def get_html_for_radio_group_headings(column_count):
+    result_html = ''
+    column_width = 100 / column_count
+    for x in range(0, column_count):
+        result_html += ('<span class="col-m" style="width: {column_width}%">'
+                            '{x}'
+                        '</span>').format(x=x, column_width=column_width)
     return result_html
