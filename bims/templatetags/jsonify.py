65e34b87aea3f019415900334d9a4ac621c8ec8a
Christiaanvdm <Christiaan@kartoza.com>
2019-01-30 14:32:19 +0200

    Added changes to radio buttons as well as styling to allign buttons with headings

--- bims/templatetags/jsonify.py
+++ bims/templatetags/jsonify.py
@@ -18,6 +18,7 @@ def jsonify(object):
 
 @register.simple_tag
 def get_html_for_radio_group(name, sass_rating):
+    column_width=100/6
     try:
         value = int(sass_rating[-1])
     except:
@@ -26,21 +27,21 @@ def get_html_for_radio_group(name, sass_rating):
                 '<div class="form-control sass-radio-label" '
                 'name="{name}">').format(name = name)
     for x in range(0, 6):
-        if x == (value - 1):
+        if x == (value):
             checked = 'checked'
         else:
             checked = ''
-        if x == 0:
-            starting_space = ''
-        else:
-            starting_space = '&nbsp;&nbsp;&nbsp;'
-        result_html += ('{starting_space}{x}&nbsp;<input type="radio" '
-                        'name="{name}" value="{x}" '
-                        '{checked}>').format(
-                                        name=name,
-                                        checked=checked,
-                                        x = x,
-                                        starting_space=starting_space)
+        next_radio_button = ('<input type="radio" '
+                            'name="{name}" value="{x}" '
+                            '{checked}>').format(
+                                            name=name,
+                                            checked=checked,
+                                            x = x)
+
+        result_html += ('<span class="col-m" style="width: {column_width}%">'
+                            '{next_radio_button}'
+                        '</span>').format(next_radio_button=next_radio_button,
+                                            column_width=column_width)
     result_html += '</div>'
     return result_html
 
