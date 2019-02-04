06e652cb2cd2ddc68f1c2b045381e8f4466c54ed
Dimas <dimas.ciputra@gmail.com>
2019-02-04 11:07:36 +0700

    Show location site point

--- bims/static/js/fish_form.js
+++ bims/static/js/fish_form.js
@@ -1,16 +1,41 @@
 $(function () {
+    let locationSiteCoordinate = ol.proj.transform([
+            parseFloat(location_site_long),
+            parseFloat(location_site_lat)],
+        'EPSG:4326',
+        'EPSG:3857');
+    let markerStyle = new ol.style.Style({
+        image: new ol.style.Icon(({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: '/static/img/map-marker.png'
+        }))
+    });
+    let markerSource = new ol.source.Vector();
+    let iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(locationSiteCoordinate),
+    });
+    markerSource.addFeature(iconFeature);
+
     let map = new ol.Map({
         target: 'fish-map',
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
-            })
+            }),
+            new ol.layer.Vector({
+                source: markerSource,
+                style: markerStyle,
+            }),
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([37.41, 8.82]),
-            zoom: 4
+            center: locationSiteCoordinate,
+            zoom: 11
         })
     });
+
     $("#date").datepicker({
         changeMonth: true,
         changeYear: true
--- bims/views/fish_form.py
+++ bims/views/fish_form.py
@@ -1,13 +1,29 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
+from bims.models import LocationSite
 
 
 class FishFormView(TemplateView):
     """View for fish form"""
     template_name = 'fish_form_page.html'
+    location_site = None
+
+    def get_context_data(self, **kwargs):
+        context = super(FishFormView, self).get_context_data(**kwargs)
+        context['location_site_name']  = self.location_site.name
+        context['location_site_lat'] = self.location_site.get_centroid().y
+        context['location_site_long'] = self.location_site.get_centroid().x
+        return context
 
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
-        site_id = request.GET.get('site_id', None)
+        site_id = request.GET.get('siteId', None)
+        if site_id:
+            self.location_site =  get_object_or_404(
+                LocationSite,
+                pk=site_id
+            )
+
         return super(FishFormView, self).get(request, *args, **kwargs)
