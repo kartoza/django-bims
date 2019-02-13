7229fbd1cba587f1ac7fdd2b32ebe0858007185f
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-02-13 15:19:05 +0700

    Initial work on sass single site detailed dashboard (#795)
    * Site visit taxon chart
    
    * Add SASS Taxa per biotope
    
    * Add sensitivity chart
    
    * Update sass taxa per biotope
    
    * Show sass chart per site visit
    
    * Added graticule on map
    
    * Added scaleline control
    
    * Add marker to map
    
    * Add biotope ratings chart
    
    * Check url
    
    * Add additional information

---
+++ sass/static/js/sass_dashboard.js
@@ -0,0 +1,355 @@
+function drawMap() {
+    let scaleLineControl = new ol.control.ScaleLine();
+    let map = new ol.Map({
+        controls: ol.control.defaults().extend([
+            scaleLineControl
+        ]),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM({
+                    wrapX: false
+                })
+            })
+        ],
+        target: 'map',
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+
+    let graticule = new ol.Graticule({
+        strokeStyle: new ol.style.Stroke({
+            color: 'rgba(255,120,0,0.9)',
+            width: 2,
+            lineDash: [0.5, 4]
+        }),
+        showLabels: true
+    });
+
+    graticule.setMap(map);
+
+    // Map marker
+    let iconFeatures = [];
+    let iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.transform(coordinates, 'EPSG:4326', 'EPSG:3857')),
+        name: siteCode,
+    });
+    iconFeatures.push(iconFeature);
+    let vectorSource = new ol.source.Vector({
+        features: iconFeatures
+    });
+    let iconStyle = new ol.style.Style({
+        image: new ol.style.Icon(({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: '/static/img/map-marker.png'
+        }))
+    });
+    let vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: iconStyle
+    });
+    map.addLayer(vectorLayer);
+    map.getView().fit(vectorSource.getExtent(), map.getSize());
+    map.getView().setZoom(10);
+}
+
+function renderSASSSummaryChart() {
+    let data = {
+        'labels': dateLabels,
+        'datasets': [{
+            'label': 'SASS Scores',
+            'data': sassScores,
+            'backgroundColor': '#589f48',
+            'fill': 'false',
+        }]
+    };
+    let taxaNumberData = {
+        'labels': dateLabels,
+        'datasets': [{
+            'label': 'Number of Taxa',
+            'data': taxaNumbers,
+            'backgroundColor': '#589f48',
+            'fill': 'false',
+        }]
+    };
+    let asptData = {
+        'labels': dateLabels,
+        'datasets': [{
+            'label': 'ASPT',
+            'data': asptList,
+            'backgroundColor': '#589f48',
+            'fill': 'false',
+        }]
+    };
+    let scalesOption = {
+        xAxes: [{
+            display: false
+        }],
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    };
+    let options = {
+        scales: scalesOption
+    };
+    let sassScoreChart = new Chart($('#sass-score-chart'), {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+    let taxaNumberChart = new Chart($('#taxa-numbers-chart'), {
+        type: 'bar',
+        data: taxaNumberData,
+        options: options
+    });
+    let asptChart = new Chart($('#aspt-chart'), {
+        type: 'bar',
+        data: asptData,
+        options: {
+            scales: scalesOption,
+            tooltips: {
+                callbacks: {
+                    label: function (tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return 'ASPT : ' + tooltipItem.yLabel.toFixed(2);
+                    }
+                }
+            }
+        }
+    });
+}
+
+function renderSASSTaxonPerBiotope() {
+    let sassTaxon = {};
+    let totalSass = {
+        'stone': 0,
+        'veg': 0,
+        'gravel': 0,
+        'site': 0
+    };
+    let numberOfTaxa = {
+        'stone': 0,
+        'veg': 0,
+        'gravel': 0,
+        'site': 0,
+    };
+
+    let table = $('#sass-taxon-per-biotope');
+    $.each(sassTaxonData, function (index, value) {
+        let $tr = $('<tr data-id="' + value['sass_taxon_id'] + '" data-weight="' + value['sass_score'] + '">');
+        if (sassTaxon.hasOwnProperty(value['taxonomy__canonical_name'])) {
+            $tr.append('<td></td>');
+            $tr.insertAfter(sassTaxon[value['taxonomy__canonical_name']]);
+        } else {
+            sassTaxon[value['taxonomy__canonical_name']] = $tr;
+            $tr.append('<td>' +
+                value['taxonomy__canonical_name'] +
+                '</td>');
+            table.append($tr);
+        }
+        sassTaxon[value['taxonomy__canonical_name']] = $tr;
+        $tr.append('<td>' +
+            value['sass_taxon_name'] +
+            '</td>');
+        $tr.append('<td>' +
+            value['sass_score'] +
+            '</td>');
+        $tr.append('<td class="stone">' +
+            '</td>');
+        $tr.append('<td class="veg">' +
+            '</td>');
+        $tr.append('<td class="gravel">' +
+            '</td>');
+        $tr.append('<td class="gravel">' +
+            value['taxon_abundance__abc'] +
+            '</td>');
+        totalSass['site'] += parseInt(value['sass_score']);
+        numberOfTaxa['site'] += 1;
+    });
+
+    // SASS Score total
+    let $sassScoreTr = $('<tr class="total-table" id="sass-score-total">');
+    $sassScoreTr.append('<td colspan="3">SASS Score</td>');
+    $sassScoreTr.append('<td class="stone">-</td>');
+    $sassScoreTr.append('<td class="veg">-</td>');
+    $sassScoreTr.append('<td class="gravel">-</td>');
+    $sassScoreTr.append('<td class="site">-</td>');
+    table.append($sassScoreTr);
+
+    // Number of taxa
+    let $numberTaxaTr = $('<tr class="total-table" id="number-taxa">');
+    $numberTaxaTr.append('<td colspan="3">Number of Taxa</td>');
+    $numberTaxaTr.append('<td class="stone">-</td>');
+    $numberTaxaTr.append('<td class="veg">-</td>');
+    $numberTaxaTr.append('<td class="gravel">-</td>');
+    $numberTaxaTr.append('<td class="site">-</td>');
+    table.append($numberTaxaTr);
+
+    // ASPT
+    let $asptTr = $('<tr class="total-table" id="total-aspt">');
+    $asptTr.append('<td colspan="3">ASPT</td>');
+    $asptTr.append('<td class="stone">-</td>');
+    $asptTr.append('<td class="veg">-</td>');
+    $asptTr.append('<td class="gravel">-</td>');
+    $asptTr.append('<td class="site">-</td>');
+    table.append($asptTr);
+
+    $.each(biotopeData, function (index, value) {
+        let sassTaxonId = value['sass_taxon'];
+        let $tr = table.find("[data-id='" + sassTaxonId + "']");
+        if (!$tr) {
+            return true;
+        }
+        let lowercaseValue = value['biotope__name'].toLowerCase();
+        let biotope = '';
+        if (lowercaseValue.includes('vegetation')) {
+            biotope = 'veg';
+        } else if (lowercaseValue.includes('stone')) {
+            biotope = 'stone';
+        } else {
+            biotope = 'gravel';
+        }
+        let $td = $tr.find('.' + biotope);
+        if ($tr.data('weight')) {
+            totalSass[biotope] += parseInt($tr.data('weight'));
+            numberOfTaxa[biotope] += 1;
+        }
+        $td.html(value['taxon_abundance__abc']);
+    });
+
+    $.each(totalSass, function (index, value) {
+        $sassScoreTr.find('.' + index).html(value);
+        $numberTaxaTr.find('.' + index).html(numberOfTaxa[index]);
+        if (value && numberOfTaxa[index]) {
+            $asptTr.find('.' + index).html(parseFloat(value / numberOfTaxa[index]).toFixed(2))
+        }
+    });
+}
+
+function renderSensitivityChart() {
+    let options = {};
+    let data = {
+        datasets: [{
+            data: [
+                sensitivityChartData['highly_sensitive'],
+                sensitivityChartData['sensitive'],
+                sensitivityChartData['tolerant'],
+                sensitivityChartData['highly_tolerant']
+            ],
+            backgroundColor: [
+                "#4dcfff",
+                "#5cff8b",
+                "#ffee66",
+                "#ff5d49"
+            ]
+        }],
+        labels: [
+            'Highly Sensitive',
+            'Sensitive',
+            'Tolerant',
+            'Highly Tolerant'
+        ],
+    };
+    let sensitivityChart = new Chart($('#sensitivity-chart'), {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+}
+
+function renderBiotopeRatingsChart() {
+    let barOptions_stacked = {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                },
+                gridLines: {},
+                stacked: true
+            }],
+            yAxes: [{
+                barPercentage: 1,
+                gridLines: {
+                    display: false,
+                    color: "#fff",
+                },
+                stacked: true
+            }]
+        }
+    };
+
+    let ctx = document.getElementById("biotope-ratings-chart");
+
+    let labels = [];
+    let datasets = {};
+    let datasetsList = [];
+
+    let color = {
+        'Stones in current (SIC)': '#1F4E7A',
+        'Stones out of current (SOOC)' : '#2E76B6',
+        'Aquatic vegetation': '#375822',
+        'Gravel': '#4E7F31',
+        'Sand': '#BE9001',
+        'Silt/mud/clay': '#bdbe0d'
+    };
+
+    $.each(dateLabels, function (index, date) {
+        if (!biotopeRatingData.hasOwnProperty(date)) {
+            return true;
+        }
+        let data = biotopeRatingData[date];
+        labels.push(date);
+        $.each(biotopeRatingLabels, function (index, biotopeName) {
+            let ratingNumber = 0;
+            let datasetsIndex = 0;
+            if (data.hasOwnProperty(biotopeName)) {
+                ratingNumber = parseInt(data[biotopeName]);
+            }
+            if (!datasets.hasOwnProperty(biotopeName)) {
+                let backgroundColor = "rgba(63,103,126,1)";
+                if (color.hasOwnProperty(biotopeName)) {
+                    backgroundColor = color[biotopeName];
+                }
+                datasetsList.push({
+                    'label': biotopeName,
+                    'data': [],
+                    backgroundColor: backgroundColor
+                });
+                datasetsIndex = datasetsList.length - 1;
+                datasets[biotopeName] = datasetsIndex
+            } else {
+                datasetsIndex = datasets[biotopeName];
+            }
+            datasetsList[datasetsIndex]['data'].push(ratingNumber);
+        });
+    });
+
+    let biotopeRatingsChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: labels,
+            datasets: datasetsList
+        },
+        options: barOptions_stacked,
+    });
+}
+
+$(function () {
+    drawMap();
+    renderSASSSummaryChart();
+    renderSASSTaxonPerBiotope();
+    renderSensitivityChart();
+    renderBiotopeRatingsChart();
+
+    if (dateLabels) {
+        $('#earliest-record').html(moment(dateLabels[0], 'DD-MM-YYYY').format('MMMM D, Y'));
+        $('#latest-record').html(moment(dateLabels[dateLabels.length - 1], 'DD-MM-YYYY').format('MMMM D, Y'));
+        $('#number-of-sass-record').html(dateLabels.length);
+    }
+});
---
+++ sass/views/sass_dashboard.py
@@ -0,0 +1,220 @@
+import json
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.db.models import (
+    Case, When, F, Count, Sum, FloatField,
+    IntegerField, Q
+)
+from django.db.models.functions import Cast, Coalesce
+from bims.models.location_site import LocationSite
+from bims.api_views.search_version_2 import SearchVersion2
+from sass.models import (
+    SiteVisitTaxon,
+    SiteVisitBiotopeTaxon
+)
+
+
+class SassDashboardView(TemplateView):
+    template_name = 'sass_dashboard_single_site.html'
+    location_site = LocationSite.objects.none()
+    site_visit_taxa = SiteVisitTaxon.objects.none()
+
+    def get_site_visit_taxon(self):
+        filters = self.request.GET
+        search = SearchVersion2(filters)
+        collection_records = search.process_search()
+        self.site_visit_taxa = SiteVisitTaxon.objects.filter(
+            id__in=collection_records
+        )
+
+    def get_sass_score_chart_data(self):
+        data = {}
+        summary = self.site_visit_taxa.annotate(
+            date=F('site_visit__site_visit_date'),
+        ).values('date').annotate(
+            count=Count('sass_taxon'),
+            sass_score=Case(
+                When(site_visit__sass_version=5, then=Sum(
+                    'sass_taxon__sass_5_score')),
+                When(site_visit__sass_version=4, then=Sum(
+                    'sass_taxon__score')),
+                default=Sum('sass_taxon__sass_5_score')),
+        ).annotate(
+            aspt=Cast(F('sass_score'), FloatField()) / Cast(F('count'),
+                                                            FloatField()),
+        ).order_by('date')
+
+        data['date_labels'] = (
+            [dt.strftime('%d-%m-%Y')
+             for dt in summary.values_list('date', flat=True)])
+        data['taxa_numbers'] = list(
+            summary.values_list('count', flat=True))
+        data['sass_scores'] = list(
+            summary.values_list('sass_score', flat=True))
+        data['aspt_list'] = list(
+            summary.values_list('aspt', flat=True))
+        return data
+
+    def get_sass_taxon_table_data(self):
+        data = {}
+        latest_site_visit = (
+            self.site_visit_taxa.
+            order_by('-site_visit__site_visit_date')[0].site_visit
+        )
+        sass_taxon_data = (
+            self.site_visit_taxa.filter(
+                site_visit=latest_site_visit
+            ).annotate(
+                sass_taxon_name=Case(
+                    When(site_visit__sass_version=5,
+                         then='sass_taxon__taxon_sass_5'),
+                    When(site_visit__sass_version=4,
+                         then='sass_taxon__taxon_sass_4'),
+                    default='sass_taxon__taxon_sass_4'
+                ),
+                sass_score=Case(
+                    When(site_visit__sass_version=5,
+                         then='sass_taxon__sass_5_score'),
+                    When(site_visit__sass_version=4,
+                         then='sass_taxon__score'),
+                    default='sass_taxon__sass_5_score'
+                ),
+            ).values(
+                'taxonomy__canonical_name',
+                'taxon_abundance__abc',
+                'sass_taxon_name',
+                'sass_score',
+                'sass_taxon_id')
+            .order_by('sass_taxon_name')
+            .distinct('sass_taxon_name')
+        )
+
+        biotope_data = (
+            SiteVisitBiotopeTaxon.objects.filter(
+                sass_taxon__in=self.site_visit_taxa.values_list('sass_taxon'),
+                site_visit=latest_site_visit).values(
+                'biotope__name', 'sass_taxon', 'taxon_abundance__abc')
+        )
+        data['sass_taxon_data'] = json.dumps(list(sass_taxon_data))
+        data['biotope_data'] = json.dumps(list(biotope_data))
+        data['site_visit_date'] = latest_site_visit.site_visit_date
+        return data
+
+    def get_sensitivity_chart_data(self):
+        # Ordered by
+        # Highly tolerant = 1 - 3
+        # Tolerant = 4 - 7
+        # Sensitive = 8 - 11
+        # Highly Sensitive 12 - 15
+        sensitivity_data = (
+            self.site_visit_taxa.annotate(
+                sass_score=Case(
+                    When(site_visit__sass_version=5,
+                         then='sass_taxon__sass_5_score'),
+                    When(site_visit__sass_version=4,
+                         then='sass_taxon__score'),
+                    default='sass_taxon__sass_5_score',
+                    output_field=IntegerField()
+                ),
+            ).annotate(
+                highly_tolerant_value=Case(
+                    When(
+                        condition=Q(
+                            sass_score__gte=1, sass_score__lte=3),
+                        then=1),
+                    output_field=IntegerField()
+                ),
+                tolerant_value=Case(
+                    When(
+                        condition=Q(
+                            sass_score__gte=4, sass_score__lte=7),
+                        then=1),
+                    output_field=IntegerField()
+                ),
+                sensitive_value=Case(
+                    When(
+                        condition=Q(
+                            sass_score__gte=8, sass_score__lte=11),
+                        then=1),
+                    output_field=IntegerField()
+                ),
+                highly_sensitive_value=Case(
+                    When(
+                        condition=Q(
+                            sass_score__gte=12, sass_score__lte=15),
+                        then=1),
+                    output_field=IntegerField()
+                ),
+            ).aggregate(
+                highly_tolerant=Coalesce(Sum('highly_tolerant_value'), 0),
+                tolerant=Coalesce(Sum('tolerant_value'), 0),
+                sensitive=Coalesce(Sum('sensitive_value'), 0),
+                highly_sensitive=Coalesce(Sum('highly_sensitive_value'), 0)
+            )
+        )
+
+        return sensitivity_data
+
+    def get_biotope_ratings_chart_data(self):
+        data = {}
+
+        biotope_ratings = self.site_visit_taxa.filter(
+            site_visit__sass_biotope_fraction__sass_biotope__biotope_form=1
+        ).annotate(
+            date=F('site_visit__site_visit_date'),
+            rate=F('site_visit__sass_biotope_fraction__rate'),
+            biotope=F('site_visit__sass_biotope_fraction__sass_biotope__name')
+        ).values('date', 'rate', 'biotope').order_by(
+            'site_visit__site_visit_date',
+            'site_visit__sass_biotope_fraction__sass_biotope__display_order'
+        ).distinct()
+
+        biotope_labels = []
+
+        for rating_data in biotope_ratings:
+            date = rating_data['date'].strftime('%d-%m-%Y')
+            if date not in data:
+                data[date] = {}
+            rate = rating_data['rate']
+            biotope = rating_data['biotope'].encode('utf-8')
+            if not rate:
+                rate = 0
+            data[date][biotope] = rate
+            if biotope not in biotope_labels:
+                biotope_labels.append(biotope)
+
+        return {
+            'rating_data': data,
+            'biotope_labels': biotope_labels
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super(SassDashboardView, self).get_context_data(**kwargs)
+        context['coord'] = [
+            self.location_site.get_centroid().x,
+            self.location_site.get_centroid().y
+        ]
+        context['site_code'] = self.location_site.site_code
+        context['site_description'] = self.location_site.site_description
+        self.get_site_visit_taxon()
+
+        context['sass_score_chart_data'] = self.get_sass_score_chart_data()
+        context['sass_taxon_table_data'] = self.get_sass_taxon_table_data()
+        context['sensitivity_chart_data'] = self.get_sensitivity_chart_data()
+        context['biotope_ratings_chart_data'] = (
+            self.get_biotope_ratings_chart_data()
+        )
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        site_id = kwargs.get('site_id', None)
+        if not site_id or not request.GET or not request.GET.get(
+            'siteId', None):
+            raise Http404()
+        self.location_site = get_object_or_404(
+            LocationSite,
+            pk=site_id
+        )
+        return super(SassDashboardView, self).get(request, *args, **kwargs)
