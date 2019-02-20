3c8d0dd473319887f476c3bf3d4eee61f4e7d4c7
Dimas <dimas.ciputra@gmail.com>
2019-02-20 10:43:47 +0700

    Show filter list

--- sass/static/js/sass_dashboard_multiple.js
+++ sass/static/js/sass_dashboard_multiple.js
@@ -417,6 +417,7 @@ $(function () {
         dataType: 'json',
         success: function (data) {
             $('.ajax-container').show();
+            renderFilterList($('.filter-table'));
             renderAll(data);
         }
     });
