8ad33787872ff31b6677ca33b5fc789a54b8d1f3
Dimas <dimas.ciputra@gmail.com>
2019-02-27 12:16:00 +0700

    Update POST form

--- bims/views/fish_form.py
+++ bims/views/fish_form.py
@@ -7,7 +7,6 @@ from django.shortcuts import get_object_or_404
 from bims.models import LocationSite, Biotope
 from sass.models import SamplingMethod
 
-
 RIVER_CATCHMENT_ORDER = [
     'primary_catchment_area',
     'secondary_catchment_area',
@@ -15,6 +14,7 @@ RIVER_CATCHMENT_ORDER = [
     'quaternary_catchment_area',
     'quinary_catchment_area'
 ]
+
 
 class FishFormView(TemplateView):
     """View for fish form"""
@@ -50,7 +50,7 @@ class FishFormView(TemplateView):
         if river_catchment_value and river_catchment_query:
             taxa_list = list(
                 LocationSite.objects.filter(**{
-                    river_catchment_query:river_catchment_value
+                    river_catchment_query: river_catchment_value
                 }).values(
                     taxon_id=F('biological_collection_record__taxonomy'),
                     taxon_name=F(
@@ -65,10 +65,11 @@ class FishFormView(TemplateView):
         context = super(FishFormView, self).get_context_data(**kwargs)
         if not self.location_site:
             return context
-        context['location_site_name']  = self.location_site.name
-        context['location_site_code']  = self.location_site.site_code
+        context['location_site_name'] = self.location_site.name
+        context['location_site_code'] = self.location_site.site_code
         context['location_site_lat'] = self.location_site.get_centroid().y
         context['location_site_long'] = self.location_site.get_centroid().x
+        context['site_id'] = self.location_site.id
         context['taxa'] = list(
             self.location_site.biological_collection_record.values(
                 taxon_id=F('taxonomy'),
@@ -94,7 +95,7 @@ class FishFormView(TemplateView):
     def get(self, request, *args, **kwargs):
         site_id = request.GET.get('siteId', None)
         if site_id:
-            self.location_site =  get_object_or_404(
+            self.location_site = get_object_or_404(
                 LocationSite,
                 pk=site_id
             )
@@ -104,3 +105,27 @@ class FishFormView(TemplateView):
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         date_string = request.POST.get('date', None)
+        post_data = request.POST.dict()
+        location_site = LocationSite.objects.get(
+            post_data.get('site-id', None)
+        )
+        taxa_list = list(
+            location_site.biological_collection_record.values(
+                taxon_id=F('taxonomy'),
+                taxon_name=F(
+                    'taxonomy__'
+                    'canonical_name'),
+                rank=F('taxonomy__rank')
+            ).distinct('taxonomy')
+        )
+        for taxon in taxa_list:
+            observed_key = '{}-observed'.format(taxon.taxon_id)
+            abundance_key = '{}-abundance'.format(taxon.taxon_id)
+            sampling_method_key = '{}-sampling-method'.format(
+                taxon.taxon_id
+            )
+            try:
+                if post_data[observed_key]:
+                    pass
+            except KeyError:
+                continue
