6bf0fde325ee8b3bcbf7c38c91837cd1139383b4
Dimas <dimas.ciputra@gmail.com>
2019-02-27 11:58:13 +0700

    Get taxa from river catchment

--- bims/views/fish_form.py
+++ bims/views/fish_form.py
@@ -1,3 +1,4 @@
+import json
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -7,10 +8,58 @@ from bims.models import LocationSite, Biotope
 from sass.models import SamplingMethod
 
 
+RIVER_CATCHMENT_ORDER = [
+    'primary_catchment_area',
+    'secondary_catchment_area',
+    'tertiary_catchment_area',
+    'quaternary_catchment_area',
+    'quinary_catchment_area'
+]
+
 class FishFormView(TemplateView):
     """View for fish form"""
     template_name = 'fish_form_page.html'
     location_site = None
+
+    def get_taxa_from_river_catchment(self):
+        river_catchment_value = None
+        river_catchment_query = None
+        location_site_context = json.loads(self.location_site.location_context)
+        try:
+            water_group = (
+                location_site_context['context_group_values']['water_group']
+                ['service_registry_values']
+            )
+            river_catchment_order = RIVER_CATCHMENT_ORDER
+            river_catchment_order.reverse()
+            for river_catchment in river_catchment_order:
+                if river_catchment in water_group:
+                    river_catchment_value = (
+                        water_group[river_catchment]['value']
+                    )
+                    river_catchment_query = (
+                        'location_context__'
+                        'context_group_values__water_group__'
+                        'service_registry_values__%s__value' % river_catchment
+                    )
+                    break
+        except KeyError:
+            pass
+
+        taxa_list = []
+        if river_catchment_value and river_catchment_query:
+            taxa_list = list(
+                LocationSite.objects.filter(**{
+                    river_catchment_query:river_catchment_value
+                }).values(
+                    taxon_id=F('biological_collection_record__taxonomy'),
+                    taxon_name=F(
+                        'biological_collection_record__taxonomy__'
+                        'canonical_name'),
+                    rank=F('biological_collection_record__taxonomy__rank')
+                ).distinct('biological_collection_record__taxonomy')
+            )
+        return taxa_list
 
     def get_context_data(self, **kwargs):
         context = super(FishFormView, self).get_context_data(**kwargs)
