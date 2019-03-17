f348644b08c931f1f53b727cbc9f7deddeae2b16
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-03-17 17:54:29 +0700

    Order river catchment in filter list (#1008)

--- bims/api_views/spatial_scale_filter.py
+++ bims/api_views/spatial_scale_filter.py
@@ -1,6 +1,8 @@
 # coding=utf-8
+import copy
 from rest_framework.views import APIView, Response
 from bims.models import SpatialScale, SpatialScaleGroup
+from bims.views.fish_form import RIVER_CATCHMENT_ORDER
 
 
 class SpatialScaleFilterList(APIView):
@@ -37,11 +39,31 @@ class SpatialScaleFilterList(APIView):
             spatial_tree.append(spatial_tree_data)
         return spatial_tree
 
+    def order_river_catchments(self, spatial_scale_list):
+        """
+        Order river catchment filter
+        :param spatial_scale_list: list of spatial scale filter
+        """
+        river_catchment_order = copy.deepcopy(RIVER_CATCHMENT_ORDER)
+        river_catchment_order.reverse()
+        river_catchment_order.append('nfepa_wetlands')
+        for group in spatial_scale_list:
+            if group['key'] == 'water_group':
+                ordered_river = []
+                for river_order in river_catchment_order:
+                    for group_child in group['children']:
+                        if group_child['key'] == river_order:
+                            ordered_river.append(group_child)
+                            break
+                group['children'] = ordered_river
+
     def get(self, request, *args):
         spatial_scale_groups = SpatialScaleGroup.objects.filter(
             parent__isnull=True
         )
         groups = self.get_spatial_scale(spatial_scale_groups)
+        self.order_river_catchments(groups)
+
         return Response(
             groups
         )
