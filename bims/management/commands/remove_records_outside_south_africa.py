7708681ac6c024f00a936aeb7ebbb6fbdac409f7
Dimas <dimas.ciputra@gmail.com>
2019-03-17 17:16:07 +0700

    Add a command to remove records outside South Africa

---
+++ bims/management/commands/remove_records_outside_south_africa.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand
+from django.db.models import signals
+from bims.models import (
+    Boundary,
+    BiologicalCollectionRecord,
+    SearchProcess,
+    collection_post_save_handler
+)
+
+
+class Command(BaseCommand):
+    help = 'Remove records outside South Africa'
+
+    def all_boundaries(self, parents):
+        """
+        Returns all children boundaries
+        :param parents: list of parents
+        :return: list boundaries
+        """
+        boundaries = Boundary.objects.filter(
+            top_level_boundary__in=parents
+        )
+        if not boundaries:
+            return boundaries
+        else:
+            return boundaries | self.all_boundaries(boundaries)
+
+    def handle(self, *args, **options):
+        sa_boundary = Boundary.objects.filter(
+            code_name='ZAF'
+        )
+        if not sa_boundary.exists():
+            print('SA Boundary does not exist')
+            return
+
+        signals.post_save.disconnect(
+            collection_post_save_handler
+        )
+        boundaries = self.all_boundaries(
+            parents=sa_boundary
+        )
+        boundaries = boundaries.exclude(
+            geometry__isnull=True
+        )
+        polygon_union = boundaries[0].geometry
+        print('Get all SA boundaries')
+        for boundary in boundaries[1:]:
+            polygon_union = polygon_union.union(boundary.geometry)
+
+        print('Get all records outside SA')
+        bio = BiologicalCollectionRecord.objects.exclude(
+            site__geometry_point__intersects=polygon_union
+        )
+        print('Got %s records' % bio.count())
+        bio.delete()
+        SearchProcess.objects.all().delete()
+        signals.post_save.connect(
+            collection_post_save_handler
+        )
