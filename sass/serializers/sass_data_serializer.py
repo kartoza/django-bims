06b600f83a5e5625c164ae2306cd74625d56c41c
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-04-12 15:00:08 +0700

    Update SASS csv download (#1116)
    * Update SASS csv download
    
    * Add accredited

--- sass/serializers/sass_data_serializer.py
+++ sass/serializers/sass_data_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 import json
 from django.db.models import Q
 from bims.models import LocationSite
-from sass.models import SiteVisitTaxon, SiteVisitBiotopeTaxon
+from sass.models import SiteVisitTaxon, SiteVisitBiotopeTaxon, SiteVisit
 
 
 class SassDataSerializer(serializers.ModelSerializer):
@@ -109,16 +109,51 @@ class SassDataSerializer(serializers.ModelSerializer):
 
 class SassSummaryDataSerializer(serializers.ModelSerializer):
 
-    site_code = serializers.SerializerMethodField()
+    FBIS_site_code = serializers.SerializerMethodField()
     sass_score = serializers.SerializerMethodField()
-    taxa_number = serializers.SerializerMethodField()
+    number_of_taxa = serializers.SerializerMethodField()
     aspt = serializers.SerializerMethodField()
-    date = serializers.SerializerMethodField()
+    sampling_date = serializers.SerializerMethodField()
     assessor = serializers.SerializerMethodField()
     sass_version = serializers.SerializerMethodField()
+    site_description = serializers.SerializerMethodField()
+    river_name = serializers.SerializerMethodField()
+    latitude = serializers.SerializerMethodField()
+    longitude = serializers.SerializerMethodField()
+    time_of_day = serializers.SerializerMethodField()
+    reference_category = serializers.SerializerMethodField()
+    study_reference = serializers.SerializerMethodField()
+    accredited = serializers.SerializerMethodField()
 
-    def get_site_code(self, obj):
-        return obj['site_code']
+    def get_accredited(self, obj):
+        return '-'
+
+    def get_reference_category(self, obj):
+        return obj['reference_category']
+
+    def get_study_reference(self, obj):
+        return obj['reference']
+
+    def get_time_of_day(self, obj):
+        time = obj['time_of_day']
+        if time:
+            return time.strftime('%H:%M:%S')
+        return '-'
+
+    def get_river_name(self, obj):
+        return obj['river_name']
+
+    def get_latitude(self, obj):
+        return obj['latitude']
+
+    def get_longitude(self, obj):
+        return obj['longitude']
+
+    def get_site_description(self, obj):
+        return obj['site_description']
+
+    def get_FBIS_site_code(self, obj):
+        return obj['FBIS_site_code']
 
     def get_assessor(self, obj):
         return obj['assessor']
@@ -129,14 +164,14 @@ class SassSummaryDataSerializer(serializers.ModelSerializer):
     def get_sass_score(self, obj):
         return obj['sass_score']
 
-    def get_taxa_number(self, obj):
+    def get_number_of_taxa(self, obj):
         return obj['count']
 
     def get_aspt(self, obj):
         return '{0:.2f}'.format(obj['aspt'])
 
-    def get_date(self, obj):
-        site_visit_date = obj['date']
+    def get_sampling_date(self, obj):
+        site_visit_date = obj['sampling_date']
         return site_visit_date.strftime('%d-%m-%Y')
 
     def group_fields(self, context_key, site_id, result):
@@ -166,18 +201,56 @@ class SassSummaryDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = SiteVisitTaxon
         fields = [
-            'date',
-            'site_code',
-            'sass_score',
-            'taxa_number',
-            'aspt',
-            'assessor',
+            'FBIS_site_code',
+            'site_description',
+            'river_name',
+            'latitude',
+            'longitude',
+            'sampling_date',
+            'time_of_day',
+            'accredited',
             'sass_version',
+            'sass_score',
+            'number_of_taxa',
+            'aspt',
+            'reference_category',
+            'study_reference',
+            'assessor',
         ]
+
+    def biotope_fractions(self, site_visit_id, result):
+        try:
+            site_visit = SiteVisit.objects.get(id=site_visit_id)
+        except SiteVisit.DoesNotExist:
+            return result
+        biotope_fractions = site_visit.sass_biotope_fraction.all()
+        biotopes_needed = [
+            'Stones in current',
+            'Stones out of current',
+            'Marginal vegetation in current',
+            'Marginal vegetation out of current',
+            'Aquatic vegetation',
+            'Gravel',
+            'Sand',
+            'Mud'
+        ]
+        for biotope in biotopes_needed:
+            biotope_rate = '-'
+            fractions = biotope_fractions.filter(
+                sass_biotope__name__icontains=biotope
+            )
+            if fractions.exists():
+                fraction = fractions[0]
+                if fraction.rate:
+                    biotope_rate = fraction.rate.description
+            result[biotope] = biotope_rate
+        return result
 
     def to_representation(self, obj):
         result = super(SassSummaryDataSerializer, self).to_representation(obj)
         site_id = obj['site_id']
+        site_visit_id = obj['sass_id']
+        result = self.biotope_fractions(site_visit_id, result)
         result = self.group_fields('water_group', site_id, result)
         result = self.group_fields('eco_geo_group', site_id, result)
         return result
--- sass/views/download_sass_data_site.py
+++ sass/views/download_sass_data_site.py
@@ -7,7 +7,7 @@ import datetime
 from django.http.response import JsonResponse
 from django.conf import settings
 from django.db.models import (
-    Case, When, F, Count, Sum, FloatField, Q
+    Case, When, F, Count, Sum, FloatField, Q, Value
 )
 from django.db.models.functions import Cast
 from bims.api_views.search_version_2 import SearchVersion2
@@ -109,8 +109,8 @@ def download_sass_summary_data(request):
         id__in=collection_records
     )
     summary = site_visit_taxa.annotate(
-        date=F('site_visit__site_visit_date'),
-    ).values('date').annotate(
+        sampling_date=F('site_visit__site_visit_date'),
+    ).values('sampling_date').annotate(
         count=Count('sass_taxon'),
         sass_score=Sum(Case(
             When(
@@ -120,7 +120,7 @@ def download_sass_summary_data(request):
             default='sass_taxon__score'
         )),
         sass_id=F('site_visit__id'),
-        site_code=Case(
+        FBIS_site_code=Case(
             When(site_visit__location_site__site_code__isnull=False,
                  then='site_visit__location_site__site_code'),
             default='site_visit__location_site__name'
@@ -128,10 +128,21 @@ def download_sass_summary_data(request):
         site_id=F('site_visit__location_site__id'),
         assessor=F('site_visit__assessor__username'),
         sass_version=F('site_visit__sass_version'),
+        site_description=F('site_visit__location_site__site_description'),
+        river_name=Case(
+            When(site_visit__location_site__river__isnull=False,
+                 then='site_visit__location_site__river__name'),
+            default=Value('-')
+        ),
+        latitude=F('site_visit__location_site__latitude'),
+        longitude=F('site_visit__location_site__longitude'),
+        time_of_day=F('site_visit__time'),
+        reference=F('reference'),
+        reference_category=F('reference_category'),
     ).annotate(
         aspt=Cast(F('sass_score'), FloatField()) / Cast(F('count'),
                                                         FloatField()),
-    ).order_by('date')
+    ).order_by('sampling_date')
 
     if not site_visit_taxa:
         response_message = 'No SASS data'
@@ -147,9 +158,16 @@ def download_sass_summary_data(request):
     )
     headers = serializer.data[0].keys()
     rows = serializer.data
+    formatted_headers = []
+
+    # Rename headers
+    for header in headers:
+        formatted_headers.append(header.replace('_', ' ').capitalize())
+
     with open(path_file, 'wb') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer = csv.DictWriter(csv_file, fieldnames=formatted_headers)
         writer.writeheader()
+        writer.fieldnames = headers
         for row in rows:
             writer.writerow(row)
 
