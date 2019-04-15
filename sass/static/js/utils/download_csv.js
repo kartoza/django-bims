e4ffd918f6d87caa3f7b8cc98d35f3cc2055450c
Dimas <dimas.ciputra@gmail.com>
2019-04-15 12:50:57 +0700

    Download table as png

--- sass/static/js/utils/download_csv.js
+++ sass/static/js/utils/download_csv.js
@@ -79,3 +79,38 @@ function exportTableToCSV(filename, id) {
     // Download CSV file
     downloadCSV2(csv.join("\n"), filename);
 }
+
+function onDownloadTableClicked(e) {
+    let button = $(e.target);
+    let maxAttempt = 10;
+    let attempt = 1;
+    let container = button.parent();
+    let found = false;
+    let title = button.data('download-title');
+    do {
+        if (container.hasClass('table-container')) {
+            found = true;
+        } else {
+            container = container.parent();
+            attempt++;
+        }
+    }
+    while (!found && attempt < maxAttempt);
+    if (!container) {
+        return false;
+    }
+    let table = container.find('.table');
+    table[0].scrollIntoView();
+    html2canvas(table, {
+        onrendered: function (canvas) {
+            let link = document.createElement('a');
+            link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            link.download = title + '.png';
+            link.click();
+        }
+    })
+}
+
+$(function () {
+    $('.download-table').click(onDownloadTableClicked);
+});
