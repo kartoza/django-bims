c8a1151da0258814893360ce3dc6d701587452d2
dimasciput <dimas.ciputra@gmail.com>
2019-05-04 17:23:16 +0700

    Fix flake

--- sass/views/sass_dashboard_multiple.py
+++ sass/views/sass_dashboard_multiple.py
@@ -20,12 +20,14 @@ from sass.models import (
     SassEcologicalCondition
 )
 
+
 def timing(f):
     def wrap(*args, **kwargs):
         time1 = time.time()
         ret = f(*args, **kwargs)
         time2 = time.time()
-        log('%s function took %0.3f ms' % (f.func_name, (time2-time1)*1000.0))
+        log('%s function took %0.3f ms' % (
+            f.func_name, (time2 - time1) * 1000.0))
         return ret
     return wrap
 
