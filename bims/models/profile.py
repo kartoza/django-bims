4e2ca09911a13c3aab499c59fce3f3bf82d52467
Dimas <dimas.ciputra@gmail.com>
2019-05-13 16:34:32 +0700

    Add sass_accredited time field

--- bims/models/profile.py
+++ bims/models/profile.py
@@ -1,6 +1,7 @@
 __author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
 __date__ = '26/02/18'
 
+import json
 from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 from django.db import models
@@ -38,9 +39,28 @@ class Profile(models.Model):
         verbose_name='SASS Accredited',
         default=False
     )
+    sass_accredited_time = models.DateField(
+        null=True,
+        blank=True
+    )
     hide_bims_info = models.BooleanField(
         default=False
     )
+
+    def save(self, *args, **kwargs):
+        max_allowed = 10
+        attempt = 0
+        is_dictionary = False
+
+        while not is_dictionary and attempt < max_allowed:
+            if not self.data:
+                break
+            if isinstance(self.data, dict):
+                is_dictionary = True
+            else:
+                self.data = json.loads(self.data)
+                attempt += 1
+        super(Profile, self).save(*args, **kwargs)
 
     class Meta:
         app_label = 'bims'
