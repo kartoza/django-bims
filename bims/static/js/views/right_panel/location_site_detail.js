4e49c47366d466a305bc0464dfd622bd047d74da
Dimas <dimas.ciputra@gmail.com>
2019-05-29 15:55:12 +0700

    Update single site overview panel in frontend

--- bims/static/js/views/right_panel/location_site_detail.js
+++ bims/static/js/views/right_panel/location_site_detail.js
@@ -6,6 +6,10 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
         siteId: null,
         siteName: null,
         siteDetailData: null,
+        charts: [],
+        originLegends: {},
+        endemismLegends: {},
+        consStatusLegends: {},
         apiParameters: _.template(Shared.SearchURLParametersTemplate),
         months: {
             'january': 1,
@@ -21,6 +25,13 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
             'november': 11,
             'december': 12
         },
+        chartBackgroundColours: [
+            '#8D2641',
+            '#D7CD47',
+            '#18A090',
+            '#A2CE89',
+            '#4E6440',
+            '#525351'],
         initialize: function () {
             Shared.Dispatcher.on('siteDetail:show', this.show, this);
             Shared.Dispatcher.on('siteDetail:updateCurrentSpeciesSearchResult', this.updateCurrentSpeciesSearchResult, this);
@@ -29,6 +40,9 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
             this.currentSpeciesSearchResult = newList;
         },
         show: function (id, name, zoomToObject) {
+            this.originLegends = {};
+            this.endemismLegends = {};
+            this.consStatusLegends = {};
             this.siteId = id;
             this.siteName = name;
             this.zoomToObject = zoomToObject;
@@ -174,14 +188,10 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
             chartParent.append(newCanvas);
             return document.getElementById(chartId);
         },
-
         renderMonthlyLineChart: function (data_in, chartName) {
-
             if (!(data_in.hasOwnProperty(chartName + '_chart'))) {
                 return false;
-            }
-            ;
-
+            };
             var chartConfig = {
                 type: 'line',
                 data: {
@@ -225,49 +235,6 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
             var ctx = chartCanvas.getContext('2d');
             new ChartJs(ctx, chartConfig);
         },
-
-        renderSidePanelPieChart: function (data, speciesType, chartName) {
-            var backgroundColours = [
-                '#8D2641',
-                '#D7CD47',
-                '#18A090',
-                '#A2CE89',
-                '#4E6440',
-                '#525351']
-            var originChartConfig = {
-                type: 'pie',
-                data: {
-                    datasets: [{
-                        data: data['biodiversity_data'][speciesType][chartName + '_chart']['data'],
-                        backgroundColor: backgroundColours
-                    }],
-                    labels: data['biodiversity_data'][speciesType][chartName + '_chart']['keys']
-                },
-                options: {
-                    responsive: true,
-                    legend: {display: false},
-                    title: {display: false},
-                    hover: {mode: 'nearest', intersect: false},
-                    borderWidth: 0,
-                }
-            };
-            var chartCanvas = document.getElementById(speciesType + '_' + chartName + '_chart');
-            var ctx = chartCanvas.getContext('2d');
-            new ChartJs(ctx, originChartConfig);
-
-            // Render chart labels
-            var dataKeys = data['biodiversity_data'][speciesType][chartName + '_chart']['keys'];
-            var dataLength = dataKeys.length;
-            var chart_labels = {};
-            chart_labels[chartName] = '';
-            for (var i = 0; i < dataLength; i++) {
-                chart_labels[chartName] += '<div><span style="color:' +
-                    backgroundColours[i] + ';">■</span>' +
-                    '<span style="font-style: italic;">' +
-                    dataKeys[i] + '</span></div>'
-            }
-            $('#' + chartName + '_chart_labels').html(chart_labels[chartName]);
-        },
         parseNameFromAliases: function (alias, alias_type, data) {
             var name = alias;
             var choices = [];
@@ -284,22 +251,6 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
             }
             return name;
         },
-
-        renderBiodiversityData: function (data) {
-            var $detailWrapper = $('<div></div>');
-            if (data.hasOwnProperty('biodiversity_data')) {
-                var biodiversityDataWrapper = _.template($('#biodiversity-data-template').html());
-                $detailWrapper.append(biodiversityDataWrapper({
-                    'occurrences': data['biodiversity_data']['occurrences'],
-                    'origin_data': data['biodiversity_data']['taxa'],
-                    'number_of_taxa': data['biodiversity_data']['number_of_taxa'],
-                    'ecological_condition': data['biodiversity_data']['ecological_condition'],
-                }));
-            }
-            ;
-            return $detailWrapper;
-        },
-
         showDetail: function (name, zoomToObject) {
             var self = this;
             // Render basic information
@@ -354,37 +305,22 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
                             Shared.Dispatcher.trigger('map:switchHighlight', features, true);
                         }
                     }
-                    // render site detail
-                    $('#site-detail').append(self.renderSiteDetailInfo(data));
-                    var biodiversity_fish_data = data['biodiversity_data']['fish'];
-                    var origin_length = biodiversity_fish_data['origin_chart']['keys'].length;
-                    for (let i = 0; i < origin_length; i++) {
-                        let next_name = biodiversity_fish_data['origin_chart']['keys'][i];
-                        biodiversity_fish_data['origin_chart']['keys'][i] =
-                            self.parseNameFromAliases(next_name, 'origin', data);
-                    }
-
-                    var cons_status_length = biodiversity_fish_data['cons_status_chart']['keys'].length;
-                    for (let i = 0; i < cons_status_length; i++) {
-                        let next_name = biodiversity_fish_data['cons_status_chart']['keys'][i];
-                        biodiversity_fish_data['cons_status_chart']['keys'][i] =
-                            self.parseNameFromAliases(next_name, 'cons_status', data);
-                    }
-
-                    $('#biodiversity-data').append(self.renderBiodiversityData(data));
-                    self.renderSidePanelPieChart(data, 'fish', 'origin');
-                    self.renderSidePanelPieChart(data, 'fish', 'cons_status');
-                    self.renderSidePanelPieChart(data, 'fish', 'endemism');
-
                     let sidePanelTitle = '<i class="fa fa-map-marker"></i> ' + data['site_detail_info']['fbis_site_code'];
                     if (isStaff) {
                         sidePanelTitle += '<a href="/admin/bims/locationsite/' + data['id'] + '/change/" style="float: right; padding-top: 5px">Edit</a>';
                     }
                     Shared.Dispatcher.trigger('sidePanel:updateSidePanelTitle', sidePanelTitle);
 
+                    // render site detail
+                    $('#site-detail').append(self.renderSiteDetailInfo(data));
+                    self.renderBiodiversityDataSection($('#biodiversity-data'), data);
+                    self.renderCharts();
+                    self.renderLegends(self.originLegends, $('.origin-legends'));
+                    self.renderLegends(self.endemismLegends, $('.endemism-legends'));
+                    self.renderLegends(self.consStatusLegends, $('.cons-status-legends'));
 
-                    var climateDataHTML = self.renderClimateData(data);
-                    $('#climate-data').append(climateDataHTML)
+                    let climateDataHTML = self.renderClimateData(data);
+                    $('#climate-data').append(climateDataHTML);
                     self.renderMonthlyLineChart(data['climate_data'], 'temperature');
                     self.renderMonthlyLineChart(data['climate_data'], 'rainfall');
 
@@ -395,11 +331,196 @@ define(['backbone', 'ol', 'shared', 'chartJs', 'jquery'], function (Backbone, ol
                 },
             });
         },
+        renderBiodiversityDataSection: function (container, data) {
+            let self = this;
+
+            let $sectionContainer = $('<div class="container-fluid"></div>');
+            container.append($sectionContainer);
+
+            let $table = $('<table class="table table-striped table-condensed table-sm"></table>');
+            $sectionContainer.append($table);
+
+            // create row
+            let $iconHead = $('<thead></thead>');
+            let $occurrenceRow = $('<tr></tr>');
+            let $siteRow = $('<tr></tr>');
+            let $originRow = $('<tr></tr>');
+            let $endemismRow = $('<tr></tr>');
+            let $consStatusRow = $('<tr></tr>');
+            let $numTaxaRow = $('<tr></tr>');
+            let $dashboardRow = $('<tr></tr>');
+            let $sassDashboarRow = $('<tr></tr>');
+            let $formRow = $('<tr></tr>');
+            $table.append($iconHead);
+            $table.append($occurrenceRow);
+            $table.append($siteRow);
+            $table.append($originRow);
+            $table.append($endemismRow);
+            $table.append($consStatusRow);
+            $table.append($numTaxaRow);
+            $table.append($dashboardRow);
+            $table.append($formRow);
+            $table.append($sassDashboarRow);
+
+            $iconHead.append('<td></td>');
+            $occurrenceRow.append('<td>Occurrences</td>');
+            $originRow.append('<td>Origin<div class="origin-legends"></div></td>');
+            $endemismRow.append('<td>Endemism<div class="endemism-legends"></div></td>');
+            $consStatusRow.append('<td>Cons. Status<div class="cons-status-legends"></div></td>');
+            $numTaxaRow.append('<td>Number of Taxa</td>');
+            $dashboardRow.append('<td style="padding-top: 12px;">Dashboard</td>');
+            $formRow.append('<td style="padding-top: 12px;">Form</td>');
+
+            let $sassDashboardButton = $('<button class="fbis-button-small" style="width: 100%;">SASS Dashboard</button>');
+            let $sassDashboardButtonContainer = $('<td colspan="' + (Object.keys(data['biodiversity_data']).length + 1) + '">');
+            $sassDashboarRow.append($sassDashboardButtonContainer);
+            $sassDashboardButtonContainer.append($sassDashboardButton);
+            if (data['sass_exist']) {
+                $sassDashboardButton.click(function () {
+                    let sassUrl = '/sass/dashboard/';
+                    sassUrl += self.apiParameters(filterParameters);
+                    window.location.href = sassUrl;
+                });
+            } else {
+                $sassDashboardButton.addClass('disabled');
+            }
+
+            $.each(data['biodiversity_data'], function (key, value) {
+                $iconHead.append('<td class="overview-data"><img width="30" src="/uploaded/' + value['icon'] + '"></td></td>');
+                $occurrenceRow.append('<td class="overview-data">' + value['occurrences'] + '</td>');
+                $numTaxaRow.append('<td class="overview-data">' + value['number_of_taxa'] + '</td>');
+
+                let $dashboardButton = $('<button class="fbis-button-small" style="width: 100%">' + key + '</button>');
+                if (value['occurrences'] === 0) {
+                    $dashboardButton.addClass('disabled');
+                } else {
+                    $dashboardButton.click(function () {
+                        let parameters = $.extend(true, {}, filterParameters);
+                        parameters['modules'] = value['module'];
+                        Shared.Router.updateUrl('site-detail/' + self.apiParameters(parameters).substr(1), true);
+                    });
+                }
+                let $dashboardRowContainer = $('<td>');
+                $dashboardRowContainer.append($dashboardButton);
+                $dashboardRow.append($dashboardRowContainer);
+
+                // Form button
+                let buttonName = key;
+                if (buttonName.toLowerCase().includes('invert')) {
+                    buttonName = 'SASS';
+                }
+                let $formButton = $('<button class="fbis-button-small fbis-red" style="width: 100%"> <span>Add ' + buttonName + '</span> </button>');
+                let $formRowContainer = $('<td>');
+                $formRowContainer.append($formButton);
+                $formRow.append($formRowContainer);
+                if (buttonName.toLowerCase() !== 'sass' && buttonName.toLowerCase() !== 'fish') {
+                    $formButton.addClass('disabled');
+                } else {
+                    $formButton.click(function () {
+                        let url = '#';
+                        if (buttonName.toLowerCase() === 'fish') {
+                            url = '/fish-form/?siteId=' + self.siteId;
+                        } else if (buttonName.toLowerCase() === 'sass') {
+                            url = '/sass/' + self.siteId;
+                        }
+                        window.location = url;
+                    });
+                }
+
+                let $originColumn = $('<td class="overview-data"></td>');
+                let $originCanvas = $('<canvas class="overview-chart"></canvas>');
+                $originColumn.append($originCanvas);
+                $originRow.append($originColumn);
+                self.charts.push({
+                    'canvas': $originCanvas,
+                    'data': value['origin'],
+                    'legends': self.originLegends
+                });
+
+                let $endemismColumn = $('<td class="overview-data"></td>');
+                let $endemismCanvas = $('<canvas class="overview-chart"></canvas>');
+                $endemismColumn.append($endemismCanvas);
+                $endemismRow.append($endemismColumn);
+                self.charts.push({
+                    'canvas': $endemismCanvas,
+                    'data': value['endemism'],
+                    'legends': self.endemismLegends
+                });
+
+                let $consStatusColumn = $('<td class="overview-data"></td>');
+                let $consStatusCanvas = $('<canvas class="overview-chart"></canvas>');
+                $consStatusColumn.append($consStatusCanvas);
+                $consStatusRow.append($consStatusColumn);
+                self.charts.push({
+                    'canvas': $consStatusCanvas,
+                    'data': value['cons_status'],
+                    'legends': self.consStatusLegends
+                });
+
+            });
+        },
         flatten_arr: function (arr) {
-            self = this;
+            let self = this;
             return arr.reduce(function (flat, toFlatten) {
                 return flat.concat(Array.isArray(toFlatten) ? self.flatten_arr(toFlatten) : toFlatten);
             }, []);
-        }
+        },
+        renderCharts: function () {
+            let self = this;
+            $.each(this.charts, function (index, chart) {
+                if (chart['data'].length > 0) {
+                    self.createPieChart(chart);
+                }
+            })
+        },
+        createPieChart: function (chartData) {
+            let self = this;
+            let labels = [];
+            let dataset = [];
+            let colours = [];
+            let data = chartData['data'];
+            let chartCanvas = chartData['canvas'];
+            let legends = chartData['legends'];
+            $.each(data, function (key, value) {
+                labels.push(value['name']);
+                dataset.push(value['count']);
+
+                if (legends.hasOwnProperty(value['name'])) {
+                    colours.push(legends[value['name']]);
+                } else {
+                    let length = Object.keys(legends).length;
+                    colours.push(self.chartBackgroundColours[length]);
+                    legends[value['name']] = self.chartBackgroundColours[length];
+                }
+            });
+
+            let chartConfig = {
+                type: 'pie',
+                data: {
+                    datasets: [{
+                        data: dataset,
+                        backgroundColor: colours
+                    }],
+                    labels: labels
+                },
+                options: {
+                    responsive: false,
+                    legend: {display: false},
+                    title: {display: false},
+                    hover: {mode: 'nearest', intersect: false},
+                    borderWidth: 0,
+                }
+            };
+            let ctx = chartCanvas[0].getContext('2d');
+            new ChartJs(ctx, chartConfig);
+        },
+        renderLegends: function (legends, container) {
+            $.each(legends, function (key, value) {
+                container.append('<div><span style="color:' +
+                    value + ';">■</span>' +
+                    '<span style="font-style: italic;">' +
+                    key + '</span></div>');
+            });
+        },
     })
 });
