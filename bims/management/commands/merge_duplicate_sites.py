4677b458637d6ea553c590d8e95f11d3b1d74519
dimasciput <dimas.ciputra@gmail.com>
2019-06-29 16:00:38 +0700

    Update management command

--- bims/management/commands/merge_duplicate_sites.py
+++ bims/management/commands/merge_duplicate_sites.py
@@ -1,32 +1,44 @@
-# -*- coding: utf-8 -*-
-
 from django.core.management.base import BaseCommand
-from bims.models import LocationSite, BiologicalCollectionRecord
+from django.db.models import Count
+from bims.models import (
+    LocationSite,
+    BiologicalCollectionRecord
+)
 
 
 class Command(BaseCommand):
     help = 'Merge duplicate sites'
 
     def handle(self, *args, **options):
-        all_sites = LocationSite.objects.all()
-        deleted_sites = []
-
-        for site in all_sites:
-            if site.id in deleted_sites:
-                continue
-            location_sites = LocationSite.objects.filter(
-                location_type=site.location_type,
-                geometry_point=site.geometry_point,
-                name=site.name
-            ).exclude(id=site.id)
-
-            if len(location_sites) > 0:
-                for location_site in location_sites:
-                    if location_site.id not in deleted_sites:
-                        BiologicalCollectionRecord.objects.filter(
-                            site=location_site
-                        ).update(
-                            site=site
-                        )
-                        deleted_sites.append(location_site.id)
-                        location_site.delete()
+        dupe_sites = LocationSite.objects.values(
+            'latitude',
+            'longitude',
+            'site_code'
+        ).annotate(
+            Count('id')
+        ).order_by().filter(id__count__gt=1)
+        for dupe_site in dupe_sites:
+            sites = LocationSite.objects.filter(
+                latitude=dupe_site['latitude'],
+                longitude=dupe_site['longitude']
+            )
+            site_destination = None
+            all_collections = []
+            for site in sites:
+                if not site_destination:
+                    site_destination = site
+                else:
+                    if len(site.site_description) > len(
+                            site_destination.site_description):
+                        site_destination = site
+                all_collections.append(
+                    BiologicalCollectionRecord.objects.filter(
+                        site=site
+                    )
+                )
+            for collections in all_collections:
+                collections.update(site=site_destination)
+            for site in sites:
+                if site != site_destination:
+                    site.delete()
+            print(sites.count())
