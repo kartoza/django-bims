6ef08d622eb6dd152f9ea974f6152d005c35957c
ann26 <anita@kartoza.com>
2019-09-17 13:06:55 +0700

    send confirmation email upon creating a new user

--- bims/models/auth.py
+++ bims/models/auth.py
@@ -5,6 +5,9 @@ import logging
 
 from django.contrib.auth.signals import user_logged_out
 from django.conf import settings
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
 from geonode.decorators import on_ogc_backend
 from geonode import geoserver
@@ -93,3 +96,49 @@ def do_logout(sender, user, request, **kwargs):
 
 
 user_logged_out.connect(do_logout)
+
+
+@receiver(user_signed_up, dispatch_uid="user_signed_up")
+def user_signed_up_(request, user, **kwargs):
+    username = request.POST.get('username')
+    password = request.POST.get('password1')
+    user_email = request.POST.get('email')
+    try:
+        bims_site_name = settings.BIMS_SITE_NAME
+    except AttributeError:
+        bims_site_name = 'Biodiversity Information System'
+
+    try:
+        bims_team_name = settings.BIMS_TEAM_NAME
+    except AttributeError:
+        bims_team_name = 'BIMS'
+
+    try:
+        bims_email = settings.BIMS_EMAIL_ADMIN
+    except AttributeError:
+        bims_email = 'admin@kartoza.com'
+
+    data = {
+        'site': bims_site_name,
+        'username': username,
+        'password': password,
+        'team_name': bims_team_name,
+        'bims_email': bims_email
+    }
+
+    send_mail(
+        '{} Registration Confirmation'.format(bims_site_name),
+        'Welcome,\n\n'
+        'Thank you for registering the {site}\n\n'
+        'Your username is {username}\n'
+        'Your password is {password}\n\n'
+        'You will receive an email as soon as you account has been activated.'
+        '\n\n'
+        'Please do not hesitate to contact us by emailing {bims_email} '
+        'should you have any questions.\n\n'
+        'Regards,\n'
+        '{team_name} Team'.format(**data),
+        bims_email,
+        [user_email],
+        fail_silently=False
+    )
