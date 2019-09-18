568a13df802c03804dd97c3c14139a14884a09c6
Anita Hapsari <anita@kartoza.com>
2019-09-18 08:43:16 +0700

    send confirmation email upon creating a new user (#1622)
    * send confirmation email upon creating a new user
    
    * change default settings to admin
    
    * change variable

--- bims/models/auth.py
+++ bims/models/auth.py
@@ -5,10 +5,16 @@ import logging
 
 from django.contrib.auth.signals import user_logged_out
 from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
 from geonode.decorators import on_ogc_backend
 from geonode import geoserver
 from oauth2_provider.models import AccessToken, get_application_model
+
+from preferences import preferences
 
 
 logger = logging.getLogger(__name__)
@@ -93,3 +99,41 @@ def do_logout(sender, user, request, **kwargs):
 
 
 user_logged_out.connect(do_logout)
+
+
+@receiver(user_signed_up, dispatch_uid="user_signed_up")
+def user_signed_up_(request, user, **kwargs):
+    username = request.POST.get('username')
+    user_email = request.POST.get('email')
+    try:
+        site = Site.objects.first()
+        bims_site_name = site.name
+    except Site.DoesNotExist:
+        bims_site_name = 'Biodiversity Information System'
+
+    bims_team_name = preferences.SiteSetting.default_team_name
+    if not bims_team_name:
+        bims_team_name = 'BIMS'
+
+    data = {
+        'site': bims_site_name,
+        'username': username,
+        'team_name': bims_team_name,
+        'bims_email': settings.SERVER_EMAIL
+    }
+
+    send_mail(
+        '{} Registration Confirmation'.format(bims_site_name),
+        'Welcome,\n\n'
+        'Thank you for registering the {site}\n\n'
+        'Your username is {username}\n\n'
+        'You will receive an email as soon as you account has been activated.'
+        '\n\n'
+        'Please do not hesitate to contact us by emailing {bims_email} '
+        'should you have any questions.\n\n'
+        'Regards,\n'
+        '{team_name} Team'.format(**data),
+        settings.SERVER_EMAIL,
+        [user_email],
+        fail_silently=False
+    )
