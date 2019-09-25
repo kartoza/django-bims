44d88a1ef4f5f72d960d9a752593035392914598
dimasciput <dimas.ciputra@gmail.com>
2019-09-25 09:42:37 +0700

    Fix csv download

--- bims/management/commands/clear_search_results.py
+++ bims/management/commands/clear_search_results.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 """Clear search results"""
-
+import os
+import shutil
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from bims.models.search_process import SearchProcess
 from bims.utils.logger import log
@@ -14,3 +16,8 @@ class Command(BaseCommand):
         log('Deleting all search results...')
         all_search_process = SearchProcess.objects.all()
         all_search_process.delete()
+        csv_path = os.path.join(
+            settings.MEDIA_ROOT,
+            'csv_processed'
+        )
+        shutil.rmtree(csv_path)
