5520689b0a413da1fa0de62562c7ab37d45c7c7c
dimasciput <dimas.ciputra@gmail.com>
2020-03-03 16:42:54 +0700

    Fix algae species import

--- scripts/management/commands/import_algae_species.py
+++ scripts/management/commands/import_algae_species.py
@@ -10,12 +10,30 @@ logger = logging.getLogger('bims')
 IN_GBIF = 'In GBIF'
 TAXON_RANK = 'Taxon rank'
 TAXON = 'Taxon'
+SPECIES = 'Species'
 GENUS = 'Genus'
 FAMILY = 'Family'
 ORDER = 'Order'
+IN_SA = 'In SA'
+COMMENTS = 'Comments'
+DIVISION = 'Division'
+GROWTH_FORM = 'Growth Form'
 
 
 class Command(CsvCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-d',
+            '--import-data',
+            dest='import_date',
+            default=None,
+            help='Date of the import'
+        )
+
+    def csv_file_name(self, options):
+        self.import_date = options.get('import_date', None)
+        return 'Algae Master Species List_FINAL.csv'
 
     @property
     def csv_root_folder(self):
@@ -32,6 +50,26 @@ class Command(CsvCommand):
             )
         )
 
+    def additional_data(self, row):
+        data = dict()
+
+        # -- Division
+        data[DIVISION] = row[DIVISION]
+
+        # -- Growth Form
+        data[GROWTH_FORM] = row[GROWTH_FORM]
+
+        # -- Comments
+        data[COMMENTS] = row[COMMENTS]
+
+        # -- In SA
+        data[IN_SA] = row[IN_SA]
+
+        # -- Import date
+        data['Import Date'] = self.import_date
+
+        return data
+
     def csv_dict_reader(self, csv_reader):
         errors = []
         success = []
@@ -40,6 +78,8 @@ class Command(CsvCommand):
 
         for row in csv_reader:
             try:
+                print('---------')
+                taxonomy = None
                 if row[IN_GBIF] == 'Yes':
                     # Fetch from gbif
                     taxonomy = fetch_all_species_from_gbif(
@@ -99,7 +139,13 @@ class Command(CsvCommand):
                             parent=parent
                         )
                         success.append(taxonomy.id)
+                if taxonomy:
+                    taxonomy.additional_data = self.additional_data(
+                        row
+                    )
+                    taxonomy.save()
             except Exception as e:  # noqa
+                print(str(e))
                 errors.append({
                     'row': index,
                     'error': str(e)
