0272e098803cf88e6426731e3ee86c8ad33ea857
Dimas Ciputra <dimas.ciputra@gmail.com>
2020-04-28 16:33:45 +0700

    Update how citation displayed in citation page (#2073)

--- bims/management/commands/generate_source_reference_filter.py
+++ bims/management/commands/generate_source_reference_filter.py
@@ -1,6 +1,4 @@
-import os
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from bims.tasks.source_reference import (
     generate_source_reference_filter,
     SOURCE_REFERENCE_FILTER_FILE
