b9d19322f6a5fedf671a7fde7dc4dcbcb7c50d3a
dimasciput <dimas.ciputra@gmail.com>
2020-05-07 11:27:28 +0700

    Add station layer to third party layer panel

--- bims/static/js/views/control_panel/third_party_layer_panel.js
+++ bims/static/js/views/control_panel/third_party_layer_panel.js
@@ -7,17 +7,43 @@ define(['shared', 'backbone', 'underscore', 'jqueryUi',
         source: null,
         displayed: false,
         miniSASSSelected: false,
+        inWARDSelected: false,
+        fetchingInWARDSData: false,
+        inWARDSStationsUrl: 'http://inwards.award.org.za/app_json/stations.php?wma=%27inkomati_usuthu%27,%27limpopo%27,%27olifants_letaba%27',
         events: {
             'click .close-button': 'closeClicked',
             'click .update-search': 'updateSearch',
-            'change .mini-sass-check': 'toggleMiniSASSLayer'
+            'change .mini-sass-check': 'toggleMiniSASSLayer',
+            'change .inward-check': 'toggleInward'
         },
         initialize: function (options) {
             _.bindAll(this, 'render');
             this.map = options.map;
             this.addMiniSASSLayer();
+            this.addInWARDSLayer();
             Shared.Dispatcher.on('third_party_layers:showFeatureInfo', this.showFeatureInfo, this);
-            // this.createPolygonInteraction();
+        },
+        inWARDSStyleFunction: function (feature) {
+            let properties = feature.getProperties();
+            let color = 'gray';
+            if (properties['color']) {
+                color = properties['color'];
+            }
+            let image = new ol.style.Circle({
+                radius: 5,
+                fill: new ol.style.Fill({color: color})
+            });
+            return new ol.style.Style({
+                image: image
+            });
+        },
+        addInWARDSLayer: function () {
+            this.inWARDSLayer = new ol.layer.Vector({
+                source: null,
+                style: this.inWARDSStyleFunction
+            });
+            this.inWARDSLayer.setVisible(false);
+            this.map.addLayer(this.inWARDSLayer);
         },
         addMiniSASSLayer: function () {
             let options = {
@@ -60,48 +86,101 @@ define(['shared', 'backbone', 'underscore', 'jqueryUi',
                 $('#map-legend').find(`[data-name='${this.miniSASSLayer.getSource().getParams()['layers']}']`).hide();
             }
         },
-        showFeatureInfo: function (lon, lat, siteExist = false) {
-            if (!this.miniSASSSelected) {
+        toggleInward: function (e) {
+            let self = this;
+            this.inWARDSelected = $(e.target).is(":checked");
+
+            if (this.inWARDSelected) {
+                self.inWARDSLayer.setVisible(true);
+                // Show fetching message
+                if (!this.fetchingInWARDSData) {
+                    let fetchingMessage = $('<span class="fetching" style="font-size: 10pt; font-style: italic"> (fetching)</span>');
+                    $(e.target).parent().find('.label').append(fetchingMessage);
+                    $.ajax({
+                        type: 'GET',
+                        url: this.inWARDSStationsUrl,
+                        success: function (data) {
+                            let source = new ol.source.Vector({
+                                features: (
+                                    new ol.format.GeoJSON({featureProjection: 'EPSG:3857'})
+                                ).readFeatures(data, {featureProjection: 'EPSG:3857'})
+                            });
+                            self.inWARDSLayer.setSource(source);
+                            $(e.target).parent().find('.fetching').remove();
+                        }
+                    })
+                    this.fetchingInWARDSData = true;
+                }
+            } else {
+                self.inWARDSLayer.setVisible(false);
+            }
+        },
+        showFeatureInfo: function (lon, lat, siteExist = false, featureData = null) {
+            if (!this.miniSASSSelected && !this.inWARDSelected) {
                 return false;
             }
-            console.log('showFeatureInfo');
             lon = parseFloat(lon);
             lat = parseFloat(lat);
             const view = this.map.getView();
             const coordinate = ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
-            const source = this.miniSASSLayer.getSource();
-            const getFeatureFormat = source.getParams()['getFeatureFormat'];
-            const layerName = source.getParams()['name'];
-            const queryLayer = source.getParams()['layers'];
-            let layerSource = source.getGetFeatureInfoUrl(
-                coordinate,
-                view.getResolution(),
-                view.getProjection(),
-                {'INFO_FORMAT': getFeatureFormat}
-            );
-            layerSource += '&QUERY_LAYERS=' + queryLayer;
-            $.ajax({
-                type: 'POST',
-                url: '/get_feature/',
-                data: {
-                    'layerSource': layerSource
-                },
-                success: function (data) {
-                    if (!data) {
-                        return true;
+
+            if (this.miniSASSSelected) {
+                const source = this.miniSASSLayer.getSource();
+                const getFeatureFormat = source.getParams()['getFeatureFormat'];
+                const layerName = source.getParams()['name'];
+                const queryLayer = source.getParams()['layers'];
+                let layerSource = source.getGetFeatureInfoUrl(
+                    coordinate,
+                    view.getResolution(),
+                    view.getProjection(),
+                    {'INFO_FORMAT': getFeatureFormat}
+                );
+                layerSource += '&QUERY_LAYERS=' + queryLayer;
+                $.ajax({
+                    type: 'POST',
+                    url: '/get_feature/',
+                    data: {
+                        'layerSource': layerSource
+                    },
+                    success: function (data) {
+                        if (!data) {
+                            return true;
+                        }
+                        this.showContentToSidePanel(
+                            lon, lat, layerName, data, siteExist
+                        )
                     }
-                    if (!siteExist) {
-                        let marker = new ol.Feature({
-                            geometry: new ol.geom.Point(
-                                ol.proj.fromLonLat([lon, lat])
-                            ),
-                        });
-                        Shared.Dispatcher.trigger('sidePanel:openSidePanel', {});
-                        Shared.Dispatcher.trigger('map:switchHighlight', [marker], true);
-                    }
-                    Shared.Dispatcher.trigger('sidePanel:addContentWithTab', layerName, data);
+                });
+            } else {
+                let table = this.renderInwardsTable(featureData.getProperties());
+                this.showContentToSidePanel(
+                    lon, lat, 'Station', table.prop('outerHTML'), siteExist
+                )
+            }
+        },
+        showContentToSidePanel: function (lon, lat, panelTitle, panelContent, siteExist) {
+            if (!siteExist) {
+                let marker = new ol.Feature({
+                    geometry: new ol.geom.Point(
+                        ol.proj.fromLonLat([lon, lat])
+                    ),
+                });
+                Shared.Dispatcher.trigger('sidePanel:openSidePanel', {});
+                Shared.Dispatcher.trigger('map:switchHighlight', [marker], true);
+            }
+            Shared.Dispatcher.trigger('sidePanel:addContentWithTab', panelTitle, panelContent);
+        },
+        renderInwardsTable: function (properties) {
+            let table = $(`<table class="table table-striped"></table>`);
+            for (let key in properties) {
+                if (properties[key] !== null && properties[key].constructor !== String && properties[key].constructor !== Number) {
+                    continue;
                 }
-            });
+                let title = key;
+                title = title.replace(/_/g, ' ');
+                table.append(`<tr><td style="text-transform: capitalize; min-width: 100px;">${title}</td><td>${properties[key]}</td></tr>`);
+            }
+            return table;
         },
         render: function () {
             this.$el.html(this.template());
