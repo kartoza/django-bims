081996f465f71e32fb2cdb1acba61a1b2dd707e6
Dimas Ciputra <dimas.ciputra@gmail.com>
2020-08-20 12:55:07 +0700

    Add form to update profile (#2238)

--- bims/views/profile.py
+++ bims/views/profile.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
 from django.db.models import Q, Count
+from django.http import HttpResponseRedirect, Http404
 from bims.models.survey import Survey
 from bims.models.location_site import LocationSite
 from bims.models.biological_collection_record import BiologicalCollectionRecord
@@ -11,6 +12,25 @@ class ProfileView(DetailView):
     template_name = 'user/profile.html'
     model = get_user_model()
     slug_field = 'username'
+
+    def post(self, request, *args, **kwargs):
+        if 'slug' not in kwargs:
+            raise Http404('Missing username')
+
+        profile = self.model.objects.get(
+            username=kwargs['slug']
+        )
+        if profile != self.request.user:
+            raise Http404('Mismatch user')
+
+        profile.first_name = self.request.POST.get('first-name', '')
+        profile.last_name = self.request.POST.get('last-name', '')
+        profile.bims_profile.role = self.request.POST.get('role', '')
+        profile.organization = self.request.POST.get('organization', '')
+        profile.bims_profile.save()
+        profile.save()
+
+        return HttpResponseRedirect(request.path_info)
 
     def get_context_data(self, **kwargs):
         context = super(ProfileView, self).get_context_data(**kwargs)
