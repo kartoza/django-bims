9c7e1c1e268c42f924127802d5dce793388faa11
dimasciput <dimas.ciputra@gmail.com>
2020-09-10 14:03:25 +0700

    Add content page and menu items

--- bims/views/landing_page.py
+++ bims/views/landing_page.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from django.template.response import TemplateResponse
+from django.contrib.sites.shortcuts import get_current_site
+
 from wagtail.core.models import Site
 from wagtail.core.views import serve
 from bims_theme.models.theme import CustomTheme
@@ -9,13 +11,14 @@ def landing_page_view(request, *args, **kwargs):
     request_uri = request.get_host().split(':')
     request_host = request_uri[0]
     request_port = 80
+    current_site = get_current_site(request)
     if len(request_uri) > 1:
         request_port = request_uri[1]
     wagtail_site = Site.objects.filter(
         hostname=request_host,
         port=request_port
     )
-    if wagtail_site.exists():
+    if wagtail_site.exists() and request.get_host() != current_site.domain:
         return serve(request, '/')
 
     context = {}
--- bims_theme/models/__init__.py
+++ bims_theme/models/__init__.py
@@ -3,3 +3,4 @@ from bims_theme.models.theme import *  # noqa
 from bims_theme.models.partner import *  # noqa
 from bims_theme.models.landing_page_section import *  # noqa
 from bims_theme.models.landing_page_section_content import *  # noqa
+from bims_theme.models.menu_item import *  # noqa
