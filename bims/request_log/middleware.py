ec4f6c84b8931741e24bc2496294484da9171d98
dimasciput <dimas.ciputra@gmail.com>
2021-01-15 09:18:59 +0700

    Ignore anonymous user

--- bims/request_log/middleware.py
+++ bims/request_log/middleware.py
@@ -10,14 +10,15 @@ class RequestLogMiddleware(object):
     def process_response(self, request, response):
 
         # save log_data in some way
-        RequestLog.objects.create(
-            user=request.user,
-            remote_address=request.META['REMOTE_ADDR'],
-            server_hostname=socket.gethostname(),
-            request_path=request.get_full_path(),
-            response_status=response.status_code,
-            start_time=request.start_time,
-            run_time=time.time() - request.start_time,
-        )
+        if not request.user.is_anonymous:
+            RequestLog.objects.create(
+                user=request.user,
+                remote_address=request.META['REMOTE_ADDR'],
+                server_hostname=socket.gethostname(),
+                request_path=request.get_full_path(),
+                response_status=response.status_code,
+                start_time=request.start_time,
+                run_time=time.time() - request.start_time,
+            )
 
         return response
