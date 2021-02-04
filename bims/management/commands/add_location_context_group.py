b8fc43e871da2d2a11dc994088dc19b5acf24c24
dimasciput <dimas.ciputra@gmail.com>
2021-02-04 11:17:01 +0700

    Update location_context management command

--- bims/management/commands/add_location_context_group.py
+++ bims/management/commands/add_location_context_group.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 """Add group to location context document."""
+import ast
 from django.core.management.base import BaseCommand
 from django.db.models import signals
 from bims.utils.location_context import get_location_context_data
 from bims.models.location_context_group import (
-    LocationContextGroup,
-    location_context_group_post_save
+    LocationContextGroup
 )
 
 
@@ -35,15 +35,26 @@ class Command(BaseCommand):
             default=None,
             help='Update location context for specific site')
 
+        parser.add_argument(
+            '-sc',
+            '--site-code',
+            dest='site_code',
+            default='False',
+            help='Generate site code')
+
     def handle(self, *args, **options):
 
         signals.post_save.disconnect(
-            location_context_group_post_save,
             sender=LocationContextGroup
+        )
+
+        generate_site_code = ast.literal_eval(
+            options.get('site_code')
         )
 
         get_location_context_data(
             group_keys=options.get('groups', None),
             site_id=options.get('site_id', None),
-            only_empty=options.get('only_empty', False)
+            only_empty=options.get('only_empty', False),
+            should_generate_site_code=generate_site_code
         )
