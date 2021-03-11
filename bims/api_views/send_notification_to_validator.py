54073084dca40cb7ecfbd401e51a5c57d0aa6534
dimasciput <dimas.ciputra@gmail.com>
2021-03-11 18:00:15 +0700

    Fix add record process

--- bims/api_views/send_notification_to_validator.py
+++ bims/api_views/send_notification_to_validator.py
@@ -17,13 +17,14 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
         site = request.get_host()
         scheme = request.scheme
         pk = request.GET.get('pk', None)
-        permission = \
+        permission = (
             Permission.objects.filter(
                 content_type__app_label='bims', codename='can_validate_data')
+        )
         validators = (
             Profile.objects.filter(
-                Q(user_permissions=permission) |
-                Q(groups__permissions=permission)
+                Q(user_permissions__in=permission) |
+                Q(groups__permissions__in=permission)
             ).distinct().values_list('email', flat=True)
         )
         superusers = (
@@ -31,7 +32,9 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
                 is_superuser=True
             ).values_list('email', flat=True)
         )
-        validator_emails = list(validators) + list(superusers)
+        validator_emails = list(superusers)
+        if validators:
+            validator_emails += list(validators)
 
         if pk is not None:
             try:
@@ -45,8 +48,8 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
                     )
                     class_validators = (
                         Profile.objects.filter(
-                            Q(user_permissions=class_permission) |
-                            Q(groups__permissions=class_permission)
+                            Q(user_permissions__in=class_permission) |
+                            Q(groups__permissions__in=class_permission)
                         ).values_list('email', flat=True)
                     )
                     validator_emails += list(class_validators)
