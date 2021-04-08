41895c27a6ddd9ded833a2ad84605974873ba2ec
Dimas Ciputra <dimas.ciputra@gmail.com>
2021-04-08 13:46:27 +0700

    Author multiple select in source reference page (#2579)
    * Author multiple select in source reference page
    
    * Add js file
    
    * Remove unnecessary codes

--- bims/static/js/utils/insert_param_to_url.js
+++ bims/static/js/utils/insert_param_to_url.js
@@ -1,15 +1,28 @@
-function insertParam(key, value) {
+function insertParam(key, value, resetPage = true) {
     key = encodeURIComponent(key);
     value = encodeURIComponent(value);
     // kvp looks like ['key1=value1', 'key2=value2', ...]
     var kvp = document.location.search.substr(1).split('&');
     let i = 0;
+    let j = 0;
     for (; i < kvp.length; i++) {
         if (kvp[i].startsWith(key + '=')) {
             let pair = kvp[i].split('=');
             pair[1] = value;
             kvp[i] = pair.join('=');
             break;
+        }
+    }
+
+    if (resetPage)  {
+        for (; j < kvp.length; j++) {
+            console.log(kvp)
+            if (kvp[j].startsWith('page=')) {
+                let pair = kvp[j].split('=');
+                pair[1] = '';
+                kvp[j] = pair.join('=');
+                break;
+            }
         }
     }
     if (i >= kvp.length) {
