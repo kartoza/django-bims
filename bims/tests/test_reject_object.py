3d0c1fea5a52d11b5b1091f67f1041d21ebd76cd
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-06-09 11:38:49 +0300

    fix reject if owner is null

--- bims/tests/test_reject_object.py
+++ bims/tests/test_reject_object.py
@@ -1,17 +1,43 @@
 from django.test import TestCase
-from bims.tests.model_factories import LocationSiteF
+from rest_framework.test import APIClient
+from rest_framework import status
+from bims.tests.model_factories import LocationSiteF, UserF
 
 
 class TestRejectLocationSite(TestCase):
 
     def setUp(self):
-        pass
+        self.location_site = LocationSiteF.create()
 
     def test_reject_location_site(self):
+        client = APIClient()
+        api_url = '/api/reject-location-site/'
         site = LocationSiteF.create()
-        site.rejected = True
-        site.save()
 
+        # Cannot merge sites without log in as superuser
+        res = client.get(api_url, {'pk': site.id})
+        self.assertTrue(
+            res.status_code == status.HTTP_403_FORBIDDEN
+        )
+
+        user = UserF.create(is_superuser=True)
+        client.login(
+            username=user.username,
+            password='password'
+        )
+
+        res = client.get(api_url, {})
+
+        self.assertTrue(
+            res.status_code == status.HTTP_400_BAD_REQUEST
+        )
+
+        res = client.get(api_url, {
+            'pk': site.id
+        })
+        self.assertTrue(
+            res.status_code == status.HTTP_200_OK
+        )
         self.assertEqual(site.rejected, True)
 
 
--- bims/tests/test_validate_object.py
+++ bims/tests/test_validate_object.py
@@ -1,17 +1,47 @@
+import logging
 from django.test import TestCase
-from bims.tests.model_factories import LocationSiteF
+from rest_framework.test import APIClient
+from rest_framework import status
+from bims.tests.model_factories import LocationSiteF, UserF
+
+logger = logging.getLogger('bims')
 
 
 class TestValidateLocationSite(TestCase):
 
     def setUp(self):
-        pass
+        self.location_site = LocationSiteF.create()
 
     def test_validate_location_site(self):
+        client = APIClient()
+        api_url = '/api/validate-location-site/'
         site = LocationSiteF.create()
-        site.validated = True
-        site.save()
 
+        # Cannot merge sites without log in as superuser
+        res = client.get(api_url, {'pk': site.id})
+        logger.error("herrerererrre", res.status_code)
+
+        self.assertTrue(
+            res.status_code == status.HTTP_403_FORBIDDEN
+        )
+
+        user = UserF.create(is_superuser=True)
+        client.login(
+            username=user.username,
+            password='password'
+        )
+
+        res = client.get(api_url, {})
+        self.assertTrue(
+            res.status_code == status.HTTP_400_BAD_REQUEST
+        )
+
+        res = client.get(api_url, {
+            'pk': site.id
+        })
+        self.assertTrue(
+            res.status_code == status.HTTP_200_OK
+        )
         self.assertEqual(site.validated, True)
 
 
