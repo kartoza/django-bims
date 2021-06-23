4ab2340545f78d5edd3f185a2e705310fc692c9f
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-06-23 10:53:51 +0300

    fix tests django

--- bims/api_views/send_notification_to_validator.py
+++ bims/api_views/send_notification_to_validator.py
@@ -10,7 +10,6 @@ from rest_framework.views import APIView
 from rest_framework import status
 from geonode.people.models import Profile
 from bims.models import LocationSite
-from bims.models.biological_collection_record import BiologicalCollectionRecord
 from bims.models.survey import Survey
 
 
@@ -43,13 +42,12 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
         if pk is not None:
             if model is None:
                 try:
-                    bio_record = BiologicalCollectionRecord.objects.get(pk=pk)
+                    site_visit = Survey.objects.get(pk=pk)
 
                     try:
-                        taxon_classname = bio_record.taxonomy.class_name
                         class_permission = Permission.objects.filter(
                             content_type__app_label='bims',
-                            codename='can_validate_%s' % taxon_classname.lower()
+                            codename='can_validate_site_visit'
                         )
                         class_validators = (
                             Profile.objects.filter(
@@ -61,28 +59,30 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
                     except AttributeError:
                         pass
 
-                    validator_emails = filter(None, validator_emails)
-                    bio_record.ready_for_validation = True
-                    bio_record.save()
+                    site_visit.ready_for_validation = True
+                    site_visit.save()
+
+                    messages.add_message(
+                        request,
+                        messages.SUCCESS,
+                        'Validation notification is sent.',
+                        'site_visit_validation'
+                    )
+
                     send_mail(
-                        '[BIMS] Record is ready to be validated',
+                        'Site visit is ready to be validated',
                         'Dear Validator,\n\n'
-                        'The following object is ready to be reviewed:\n'
-                        '{}://{}/nonvalidated-list/?pk={}\n\n'
+                        'The following site visit is ready to be reviewed:\n'
+                        '{}://{}/site-visit/detail/{}/\n\n'
                         'Sincerely,\nBIMS Team.'.format(scheme, site, pk),
                         '{}'.format(settings.SERVER_EMAIL),
                         validator_emails,
                         fail_silently=False
-            try:
-                site_visit = Survey.objects.get(pk=pk)
-
-                try:
-                    class_permission = Permission.objects.filter(
-                        content_type__app_label='bims',
-                        codename='can_validate_site_visit'
                     )
+
                     return JsonResponse({'status': 'success'})
-                except BiologicalCollectionRecord.DoesNotExist:
+
+                except Survey.DoesNotExist:
                     return HttpResponse(
                         'Object Does Not Exist',
                         status=status.HTTP_400_BAD_REQUEST
@@ -90,48 +90,38 @@ class SendNotificationValidation(LoginRequiredMixin, APIView):
             else:
                 try:
                     new_site = LocationSite.objects.get(pk=pk)
+
+                    try:
+                        class_permission = Permission.objects.filter(
+                            content_type__app_label='bims',
+                            codename='can_validate_site'
+                        )
+                        class_validators = (
+                            Profile.objects.filter(
+                                Q(user_permissions__in=class_permission) |
+                                Q(groups__permissions__in=class_permission)
+                            ).values_list('email', flat=True)
+                        )
+                        validator_emails += list(class_validators)
+
+                    except AttributeError:
+                        pass
+
+                    new_site.ready_for_validation = True
+                    new_site.save()
+
+                    send_mail(
+                        '[BIMS] New site is ready to be validated',
+                        'Dear Validator,\n\n'
+                        'The following site is ready to be reviewed:\n'
+                        '{}://{}/nonvalidated-site/?pk={}\n\n'
+                        'Sincerely,\nBIMS Team.'.format(scheme, site, new_site.pk),
+                        validator_emails,
+                        fail_silently=False
+                    )
+
                 except LocationSite.DoesNotExist:
                     return HttpResponse(
                         'Site Does Not Exist',
                         status=status.HTTP_400_BAD_REQUEST
                     )
-
-                send_mail(
-                    '[BIMS] New site is ready to be validated',
-                    'Dear Validator,\n\n'
-                    'The following site is ready to be reviewed:\n'
-                    '{}://{}/nonvalidated-site/?pk={}\n\n'
-                    'Sincerely,\nBIMS Team.'.format(scheme, site, new_site.pk),
-                validator_emails = filter(None, validator_emails)
-                site_visit.ready_for_validation = True
-                site_visit.save()
-
-                messages.add_message(
-                    request,
-                    messages.SUCCESS,
-                    'Validation notification is sent.',
-                    'site_visit_validation'
-                )
-
-                send_mail(
-                    'Site visit is ready to be validated',
-                    'Dear Validator,\n\n'
-                    'The following site visit is ready to be reviewed:\n'
-                    '{}://{}/site-visit/detail/{}/\n\n'
-                    'Sincerely,\nBIMS Team.'.format(scheme, site, pk),
-                    '{}'.format(settings.SERVER_EMAIL),
-                    validator_emails,
-                    fail_silently=False
-                )
-                return JsonResponse({'status': 'success'})
-
-            except Survey.DoesNotExist:
-                return HttpResponse(
-                    'Object Does Not Exist',
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-        else:
-            return HttpResponse(
-                'Object Does Not Exist',
-                status=status.HTTP_400_BAD_REQUEST
-            )
