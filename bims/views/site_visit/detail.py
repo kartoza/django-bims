3ec292137674539f21a3ee7f91d448ccc92ae327
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-08-19 18:19:50 +0300

    remove records and site location

--- bims/views/site_visit/detail.py
+++ bims/views/site_visit/detail.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from bims.models.location_site import LocationSite
+
+from bims.models.biological_collection_record import BiologicalCollectionRecord
 from django.contrib import messages
 from django.views.generic import View
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from django.utils.decorators import method_decorator
 from django.views.generic.detail import DetailView
 from bims.views.site_visit.base import SiteVisitBaseView
 from bims.models.survey import Survey
 from bims.models.basemap_layer import BaseMapLayer
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
 
 
 class SiteVisitDetailView(SiteVisitBaseView, DetailView):
@@ -41,21 +44,23 @@ class SiteVisitDeleteView(UserPassesTestMixin, View):
         return Survey.objects.filter(
                 id=site_visit_id, ).exists()
 
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super(SiteVisitDeleteView, self).dispatch(request, *args, **kwargs)
-
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         site_visit = get_object_or_404(
             Survey,
-            id=kwargs.get('sitevisitid', None)
+            id=self.kwargs.get('sitevisitid', None)
         )
+        biological_records = BiologicalCollectionRecord.objects.filter(survey=site_visit)
+        surveys = Survey.objects.filter(site=site_visit.site)
+        if len(surveys) == 1:
+            LocationSite.objects.get(pk=site_visit.site.id).delete()
         site_visit.delete()
+        biological_records.delete()
         messages.success(
             request,
             'Site visit successfully deleted!',
             extra_tags='delete_site_visit'
         )
         redirect_url = '/site-visit/list/'
+
         return HttpResponseRedirect(redirect_url)
