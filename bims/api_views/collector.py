f9e5a65821d32ebe7b603bb1d221379d51fbad92
Dimas Ciputra <dimas.ciputra@gmail.com>
2021-09-01 10:10:23 +0700

    Fix filter owner (#2754)

--- bims/api_views/collector.py
+++ bims/api_views/collector.py
@@ -4,16 +4,17 @@ from django.http.response import HttpResponse
 from django.db.models import CharField, Value as V
 from django.db.models.functions import Concat
 from rest_framework.views import APIView
-from bims.models.biological_collection_record import BiologicalCollectionRecord
-from sass.models import SiteVisit
+from bims.models.survey import Survey
 
 
 class CollectorList(APIView):
     """API for listing all biological collection record collectors."""
 
     def get(self, request, *args):
-        owners = (
-            SiteVisit.objects.all().exclude(
+        survey_owners = (
+            Survey.objects.filter(
+                biological_collection_record__isnull=False
+            ).exclude(
                 owner__isnull=True
             ).annotate(
                 full_name=Concat(
@@ -24,25 +25,7 @@ class CollectorList(APIView):
                 'full_name'
             ).values_list('full_name', flat=True)
         )
-        collectors = (
-            BiologicalCollectionRecord.objects.filter(
-                validated=True
-            ).exclude(
-                collector__exact='',
-            ).annotate(
-                full_name=Concat(
-                    'collector_user__first_name', V(' '),
-                    'collector_user__last_name',
-                    output_field=CharField()
-                )
-            ).exclude(
-                source_collection='gbif'
-            ).distinct('full_name').order_by(
-                'full_name'
-            ).values_list('full_name', flat=True)
-        )
-        all_users = list(owners) + list(collectors)
-        all_users = list(set(all_users))
+        all_users = list(survey_owners)
         all_users.sort()
         user_index = 0
         if len(all_users) > 0:
