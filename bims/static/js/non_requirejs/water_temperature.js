48ba53fe1bc864e33eae9b34aebe6f3ba02bc9db
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-11-30 14:16:08 +0300

    add highcharts lib

---
+++ bims/static/js/non_requirejs/water_temperature.js
@@ -0,0 +1,127 @@
+let map = null;
+
+function drawMap() {
+    let scaleLineControl = new ol.control.ScaleLine();
+    map = new ol.Map({
+        controls: ol.control.defaults().extend([
+            scaleLineControl
+        ]),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM({
+                    wrapX: false
+                })
+            })
+        ],
+        target: 'map',
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+
+    let graticule = new ol.Graticule({
+        strokeStyle: new ol.style.Stroke({
+            color: 'rgba(0,0,0,1)',
+            width: 1,
+            lineDash: [2.5, 4]
+        }),
+        showLabels: true
+    });
+
+    graticule.setMap(map);
+
+    // Map marker
+    let iconFeatures = [];
+    let iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.transform(coordinates, 'EPSG:4326', 'EPSG:3857')),
+        name: siteCode,
+    });
+    iconFeatures.push(iconFeature);
+    let vectorSource = new ol.source.Vector({
+        features: iconFeatures
+    });
+    let iconStyle = new ol.style.Style({
+        image: new ol.style.Icon(({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: '/static/img/map-marker.png'
+        }))
+    });
+    let vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: iconStyle
+    });
+    map.addLayer(vectorLayer);
+    map.getView().fit(vectorSource.getExtent(), map.getSize());
+    map.getView().setZoom(10);
+}
+
+function renderWaterTemperatureChart(){
+    return Highcharts.getJSON(
+    'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/usdeur.json',
+    function (data) {
+
+        Highcharts.chart('water-temperature', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Water Temperature'
+            },
+
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperature'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'Water Temperature',
+                data: data
+            }]
+        });
+    }
+);
+}
+
+
+$(function () {
+    drawMap();
+    renderWaterTemperatureChart()
+});
