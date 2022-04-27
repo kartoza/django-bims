f5e1f2259b4e821e829deaaba5b1fa1d2d904671
dimasciput <dimas.ciputra@gmail.com>
2022-04-27 14:53:11 +0700

    Add tests

--- bims/api_views/site_code.py
+++ bims/api_views/site_code.py
@@ -2,13 +2,14 @@
 from braces.views import LoginRequiredMixin
 from rest_framework.views import APIView, Response
 
-from bims.location_site.river import fetch_river_name
 from bims.models.location_site import LocationSite, generate_site_code
 
 
 class GetSiteCode(LoginRequiredMixin, APIView):
 
     def get(self, request):
+        from bims.location_site.river import fetch_river_name
+
         lat = request.GET.get('lat', None)
         lon = request.GET.get('lon', None)
         site_id = request.GET.get('site_id', None)
---
+++ bims/tests/api/test_get_site_code.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch
+
+from bims.models.site_setting import SiteSetting
+from django.test import TestCase, override_settings
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from bims.tests.model_factories import UserF, LocationSiteF
+
+
+def mocked_fetch_river_name(latitude, longitude):
+    return 'GEOCONTEXT RIVER'
+
+def mocked_fbis_catchment_generator(location_site, lat, lon, river_name):
+    return 'TEST', 'CASE'
+
+class TestGetSiteCode(TestCase):
+
+    @patch(
+        'bims.location_site.river.fetch_river_name', mocked_fetch_river_name)
+    @patch(
+        'bims.utils.site_code.fbis_catchment_generator',
+        mocked_fbis_catchment_generator)
+    def test_get_fbis_site_code(self):
+        location_site = LocationSiteF.create()
+        api_url = reverse('get-site-code') + f'?site_id={location_site.id}'
+        site_setting = SiteSetting.objects.first()
+
+        if site_setting:
+            site_setting.site_code_generator = 'fbis'
+            site_setting.save()
+
+        client = APIClient()
+        user = UserF.create(is_superuser=True)
+        client.login(
+            username=user.username,
+            password='password'
+        )
+
+        res = client.get(api_url)
+        self.assertEqual(
+            res.status_code, status.HTTP_200_OK
+        )
+        self.assertTrue(
+            res.json()['river'],
+            'GEOCONTEXT RIVER'
+        )
