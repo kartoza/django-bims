533fc5859706959118c8fa76a8a099afeaa1405a
dimasciput <dimas.ciputra@gmail.com>
2022-05-26 18:43:49 +0700

    Add url to get token

--- mobile/urls.py
+++ mobile/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from mobile.api_views.location_site import NearestLocationSites
 
 
 urlpatterns = [
+    url(r'^api-token-auth/', obtain_auth_token, name='api_token_auth'),
     url(r'^nearest-sites/$',
         NearestLocationSites.as_view(),
         name='mobile-nearest-sites'
