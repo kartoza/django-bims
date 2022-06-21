0f4a4abfa539c1578ae33d274233f79fbfb5846c
Dimas Ciputra <dimas.ciputra@gmail.com>
2022-06-21 10:26:28 +0700

    Fix cannot update sass owner (#3120)

---
+++ sass/tests/test_sass_form.py
@@ -0,0 +1,47 @@
+from dateutil.parser import parse
+from django.test import TestCase
+from django.urls import reverse
+
+from bims.models.survey import Survey
+from bims.tests.model_factories import (
+    UserF,
+    LocationSiteF
+)
+from sass.tests.model_factories import (
+    SiteVisitF
+)
+
+
+class TestSassFormView(TestCase):
+    def setUp(self):
+        pass
+
+    def test_update_sass(self):
+        user = UserF.create()
+        self.client.login(
+            username=user.username,
+            password='password'
+        )
+        site = LocationSiteF.create()
+        sass_site_visit = SiteVisitF.create(
+            location_site=site,
+            owner=user
+        )
+        date = '2022/02/02'
+        response = self.client.post(
+            reverse('sass-update-page', kwargs={
+                'sass_id': sass_site_visit.id
+            }), {
+                'owner': user.id,
+                'date': date
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            Survey.objects.filter(
+                owner=user,
+                date=parse(date),
+                site=site
+            ).exists()
+        )
