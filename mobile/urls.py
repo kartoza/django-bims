110a2a5264e1dbeea4a2643fb5dcae87f9b1e9df
dimasciput <dimas.ciputra@gmail.com>
2022-06-29 13:11:19 +0700

    Add api to get list of taxon groups

--- mobile/urls.py
+++ mobile/urls.py
@@ -7,23 +7,29 @@ from mobile.api_views.location_site import NearestLocationSites
 from mobile.api_views.taxa import AllTaxa
 from mobile.api_views.add_site_visit import AddSiteVisit
 from mobile.api_views.add_location_site import AddLocationSiteView
+from mobile.api_views.taxon_group import TaxonGroupList
 
 
 urlpatterns = [
-    url(r'^api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    url(r'^nearest-sites/$',
-        NearestLocationSites.as_view(),
-        name='mobile-nearest-sites'
-        ),
-    url(r'^choices/', AllChoicesApi.as_view(), name='all-choices'),
-    url(r'^all-taxa/$',
-        AllTaxa.as_view(),
-        name='all-taxa'
-        ),
+    url(r'^add-location-site/$',
+        AddLocationSiteView.as_view(),
+        name='mobile-add-location-site'),
     url(r'^add-site-visit/$',
         AddSiteVisit.as_view(),
         name='mobile-add-site-visit'),
-    url(r'^add-location-site/$',
-        AddLocationSiteView.as_view(),
-        name='mobile-add-location-site')
+    url(r'^all-taxa/$',
+        AllTaxa.as_view(),
+        name='all-taxa'),
+    url(r'^api-token-auth/',
+        obtain_auth_token,
+        name='api_token_auth'),
+    url(r'^choices/',
+        AllChoicesApi.as_view(),
+        name='all-choices'),
+    url(r'^nearest-sites/$',
+        NearestLocationSites.as_view(),
+        name='mobile-nearest-sites'),
+    url(r'^taxon-group/$',
+        TaxonGroupList.as_view(),
+        name='taxon-group-list')
 ]
