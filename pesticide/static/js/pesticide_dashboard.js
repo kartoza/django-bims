9968ea104e0911a3f694641e6387ec363c1c3f82
dimasciput <dimas.ciputra@gmail.com>
2023-04-12 17:30:55 +0700

    Improve the script

--- pesticide/static/js/pesticide_dashboard.js
+++ pesticide/static/js/pesticide_dashboard.js
@@ -1,6 +1,10 @@
 let dashboardMap = null;
 let wmsUrl = 'https://maps.kartoza.com/geoserver/wms';
-let pesticideLayers = {};
+let pesticideLayers = {
+    'Algae': '',
+    'Fish': '',
+    'Invert': ''
+}
 const riskModules = {
     'Algae': 'mv_algae_risk',
     'Fish': 'mv_fish_risk',
@@ -8,6 +12,11 @@ const riskModules = {
 }
 const IGNORED_VALUE = ['fid', 'geom', 'QUATERNARY', 'Toxic Unit Score', 'risk_category']
 let QUATERNARY = '';
+let riskCategories = {
+    'mv_algae_risk': '',
+    'mv_fish_risk': '',
+    'mv_invert_risk': ''
+};
 
 function downloadPesticideData(url, filename, extension = 'csv') {
     showDownloadPopup('CSV', filename, async function () {
@@ -45,10 +54,42 @@ class RiskChart {
             'High': 'orange',
             'Very High': 'red',
         };
-
         this.RISKS = ['', 'Very Low', 'Low', 'Medium', 'High', 'Very High'];
+
+        $('.download-generic-pesticide-risk-indicator').click(() => {
+            const filename = 'Generic Pesticide Risk Indicator';
+            downloadPesticideData('/static/GenericRI.xlsx', filename, 'xlsx');
+        })
+
+        $('.download-specific-pesticide-risk-indicator').click(() => {
+            const filename = 'Specific Pesticide Risk Indicator';
+            downloadPesticideData('/static/SpecificRI.xlsx', filename, 'xlsx');
+        })
     }
 
+    downloadPesticideByQuaternary() {
+        const filename = 'Pesticide Use (' + QUATERNARY + ')';
+        downloadPesticideData('/pesticide-by-quaternary/' + QUATERNARY + '/', filename);
+    }
+
+    createDashboardMap(map = null) {
+        // Call the 'createDashboardMap' function with the map and coordinates
+        dashboardMap = createDashboardMap(map, this.coordinates);
+        for (const key of Object.keys(riskModules)) {
+            pesticideLayers[key] = new ol.layer.Tile({
+                opacity: 0.1,
+                source: new ol.source.TileWMS({
+                    url: wmsUrl,
+                    params: {
+                        'LAYERS': 'kartoza:' + riskModules[key],
+                        'TILED': true
+                    },
+                    serverType: 'geoserver'
+                })
+            })
+            dashboardMap.addLayer(pesticideLayers[key]);
+        }
+    }
     createChart() {
         const that = this;
         const highChartsData = Object.entries(this.pesticideRisk).map(([name, risk]) => {
@@ -101,40 +142,7 @@ class RiskChart {
         });
     }
 
-    createDashboardMap(map = null) {
-        // Call the 'createDashboardMap' function with the map and coordinates
-        dashboardMap = createDashboardMap(map, this.coordinates);
-        for (const key of Object.keys(riskModules)) {
-            pesticideLayers[key] = new ol.layer.Tile({
-                opacity: 0.1,
-                source: new ol.source.TileWMS({
-                    url: wmsUrl,
-                    params: {
-                        'LAYERS': 'kartoza:' + riskModules[key],
-                        'TILED': true
-                    },
-                    serverType: 'geoserver'
-                })
-            })
-            dashboardMap.addLayer(pesticideLayers[key]);
-        }
-    }
-}
-
-const riskChart = new RiskChart('container', pesticideRisk, coordinates);
-riskChart.createChart();
-riskChart.createDashboardMap();
-
-class PesticideTable {
-    constructor() {
-    }
-
-    downloadPesticideByQuaternary() {
-        const filename = 'Pesticide Use (' + QUATERNARY + ')';
-        downloadPesticideData('/pesticide-by-quaternary/' + QUATERNARY + '/', filename);
-    }
-
-    getFeatureData(layer, _coordinates, callback) {
+    getFeatureData(module, layer, _coordinates, callback) {
         const view = dashboardMap.getView();
         let that = this;
         let layerSource = layer.getSource().getGetFeatureInfoUrl(
@@ -159,11 +167,18 @@ class PesticideTable {
                         QUATERNARY = value;
                         $('.download-pesticide-by-quaternary').attr('disabled', false).click(that.downloadPesticideByQuaternary);
                     }
+                    if (key === 'risk_category') {
+                        riskCategories[riskModules[module]] = value;
+                    }
                     if (!IGNORED_VALUE.includes(key) && !isNaN(parseFloat(value))) {
                         values.push({key, value: parseFloat(value)});
                     }
                 });
                 values.sort((a, b) => b.value - a.value);
+                if (Object.keys(riskCategories).length === 3) {
+                    that.pesticideRisk = riskCategories;
+                    that.createChart();
+                }
                 callback(values);
             },
             error: function (xhr, ajaxOptions, thrownError) {
@@ -178,23 +193,18 @@ class PesticideTable {
         let _coordinates = [];
         dashboardMap.getLayers().forEach(function(layer) {
             if (layer instanceof ol.layer.Vector) {
-                _coordinates = layer.getSource().getFeatures()[0].getGeometry().getCoordinates();
+                _coordinates = ol.proj.transform(coordinates, 'EPSG:4326', 'EPSG:3857');
                 return false;
             }
         });
         for (const key of Object.keys(pesticideLayers)) {
             const layer = pesticideLayers[key];
-            this.getFeatureData(layer, _coordinates, (values) => {
+            this.getFeatureData(key, layer, _coordinates, (values) => {
                 if (values) {
                     const top10 = values.slice(0, 10);
-                    const tableClass = $('.top-10-pesticides')
-                    tableClass.append(
-                        `<tr>
-                            <th colspan="2">${key}</th>
-                        </tr>`
-                    )
+                    const tableClass = $(`.top-10-pesticides .${key}-pesticide`)
                     for (const pesticide of top10) {
-                        tableClass.append(
+                        tableClass.after(
                             `<tr>
                                 <td>${pesticide.key}</td>
                                 <td>${pesticide.value}</td>
@@ -207,15 +217,6 @@ class PesticideTable {
     }
 }
 
-const pesticideTable = new PesticideTable();
+const pesticideTable = new RiskChart('container', pesticideRisk, coordinates);
+pesticideTable.createDashboardMap();
 pesticideTable.generatesTable();
-
-$('.download-generic-pesticide-risk-indicator').click(() => {
-    const filename = 'Generic Pesticide Risk Indicator';
-    downloadPesticideData('/static/GenericRI.xlsx', filename, 'xlsx');
-})
-
-$('.download-specific-pesticide-risk-indicator').click(() => {
-    const filename = 'Specific Pesticide Risk Indicator';
-    downloadPesticideData('/static/SpecificRI.xlsx', filename, 'xlsx');
-})
