918ed198001361c5b235d12271ced1c1b2089615
Dimas Ciputra <dimas.ciputra@gmail.com>
2023-07-28 09:29:02 +0700

    Save centroid of geometry when the geometry is not a point (#3423)
    * Save centroid of geometry when the geometry is not a point
    
    * Fix test

--- bims/static/js/forms/location-site-admin-form.js
+++ bims/static/js/forms/location-site-admin-form.js
@@ -1,10 +1,9 @@
 (function ($) {
     $(document).ready(function () {
-        var $geometryInput = $('[class*="geometry_"]');
+        var $geometryInput = $('[class*="geometry_"]').not(".geometry_point");
         var init = true;
         $geometryInput.hide();
         function resetAllGeometries() {
-            geodjango_geometry_point.clearFeatures();
             geodjango_geometry_line.clearFeatures();
             geodjango_geometry_polygon.clearFeatures();
             geodjango_geometry_multipolygon.clearFeatures();
@@ -18,14 +17,14 @@
             if ($(this).val()) {
                 $.get("/api/location-type/" + $(this).val() + '/allowed-geometry/', function (data) {
                     var className = 'geometry_' + data.toLowerCase();
-                    $geometryInput.not('.' + className).hide();
+                    $geometryInput.not('.' + className).not(".geometry_point").hide();
                     if (!$('.' + className).is(":visible")) {
                         $('.' + className).show();
                     }
                 })
-                    .fail(function () {
-                        console.log('fail');
-                    })
+                .fail(function () {
+                    console.log('fail');
+                })
             } else {
                 $geometryInput.hide();
             }
