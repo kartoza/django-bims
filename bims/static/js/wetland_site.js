8161fba001eea788c991baf6a566f2b4937f6d0d
Dimas <dimas.ciputra@gmail.com>
2023-08-25 13:56:29 +0700

    Make update coordinate works

--- bims/static/js/wetland_site.js
+++ bims/static/js/wetland_site.js
@@ -1,23 +1,155 @@
 let wetlandData = null;
+let wmsSource = null;
+let wmsLayer = null;
+let wmsLayerName = 'kartoza:nwm6_beta_v3_20230714';
+
+updateCoordinate = function (zoomToMap = true) {
+  try {
+    let latitude = parseFloat($('#latitude').val());
+    let longitude = parseFloat($('#longitude').val());
+    let _coordinates = ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857');
+    mapClicked(_coordinates);
+  } catch (e) {
+    return false;
+  }
+};
+
+const getFeature = (layerSource, coordinates, renderResult) => {
+  let _coordinates = ol.proj.transform(coordinates, 'EPSG:3857', 'EPSG:4326');
+  if (markerSource) {
+    markerSource.clear();
+  }
+  $('#latitude').val('');
+  $('#longitude').val('');
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: 'POST',
+      url: '/get_feature/',
+      data: {
+        'layerSource': layerSource
+      },
+      success: function (result) {
+        const data = result['feature_data'];
+        let objectData = {};
+        if (data.constructor === Object) {
+          objectData = data;
+        } else {
+          try {
+            objectData = JSON.parse(data);
+          } catch (e) {
+            console.log(e);
+            reject(e);
+            return;
+          }
+        }
+        let features = objectData['features'];
+        if (features.length > 0) {
+          const feature = features[0];
+          const bbox = feature['bbox'];
+
+          map.getView().fit(bbox, {size: map.getSize()});
+          if (renderResult) {
+            $('#latitude').val(_coordinates[1]);
+            $('#longitude').val(_coordinates[0]);
+
+            $('#fetch-river-name').attr('disabled', false);
+            $('#fetch-geomorphological-zone').attr('disabled', false);
+            $('#wetland-site-code').attr('disabled', false);
+
+            $('#site_code').val('');
+            $('#geomorphological_zone').val('');
+
+            let olFeature = new ol.format.GeoJSON().readFeatures(feature);
+
+            const format = new ol.format.GeoJSON();
+            const featureObj = format.readFeature(feature, {
+              dataProjection: 'EPSG:3857',
+              featureProjection: 'EPSG:3857'
+            })
+            const geometry = featureObj.getGeometry();
+            geometry.transform('EPSG:3857', 'EPSG:4326');
+
+            const convertedGeojsonStr = format.writeFeature(featureObj, {
+              dataProjection: 'EPSG:4326',
+              featureProjection: 'EPSG:4326'
+            });
+
+            $('#site-geometry').val(convertedGeojsonStr);
+            // vectorLayer.getSource().clear()
+            // vectorLayer.getSource().addFeatures(olFeature);
+            wetlandData = feature['properties'];
+            $('#additional-data').val(JSON.stringify(wetlandData));
+            $('#river_name').val(wetlandData['name'] ? wetlandData['name'] : '-');
+
+            moveMarkerOnTheMap(_coordinates[1], _coordinates[0], false);
+          }
+        }
+        resolve(features);
+      },
+      error: function (err) {
+        console.log(err);
+        reject(err);
+      }
+    });
+  });
+};
+
+
+let mapClicked = (coordinate) => {
+  if (!wmsLayer) return;
+
+  let view = map.getView();
+  let layerSource = wmsLayer.getSource().getGetFeatureInfoUrl(
+      coordinate,
+      view.getResolution(),
+      view.getProjection(),
+      {'INFO_FORMAT': 'application/json'}
+  );
+  layerSource += '&QUERY_LAYERS=' + wmsLayerName;
+  const zoomLevel = map.getView().getZoom();
+  $('#map-loading').css('display', 'flex');
+  getFeature(layerSource, coordinate,zoomLevel > 10).then(function(features) {
+    if (features.length > 0 && zoomLevel <= 10) {
+      let bbox = features[0]['bbox'];
+      let centroidX = (bbox[0] + bbox[2]) / 2;
+      let centroidY = (bbox[1] + bbox[3]) / 2;
+      let point = [centroidX, centroidY];
+
+      layerSource = wmsLayer.getSource().getGetFeatureInfoUrl(
+          point,
+          view.getResolution(),
+          view.getProjection(),
+          {'INFO_FORMAT': 'application/json'}
+      );
+      layerSource += '&QUERY_LAYERS=' + wmsLayerName;
+      getFeature(layerSource, coordinate, true).then(function (f) {
+        $('#map-loading').hide();
+      })
+    } else {
+      $('#map-loading').hide();
+    }
+  }).catch(function(error) {
+    // handle any errors
+  });
+}
 
 let mapReady = (map) => {
 
   // Add wetland layer
   let geoserverWms = 'https://maps.kartoza.com/geoserver/wms'
-  let wmsLayerName = 'kartoza:nwm6_beta_v3_20230714';
 
    // Create a new WMS source
-  let wmsSource = new ol.source.TileWMS({
+  wmsSource = new ol.source.TileWMS({
     url: geoserverWms,
     params: {'LAYERS': wmsLayerName},
     serverType: 'geoserver',
   });
 
   // Create a new tile layer with the WMS source
-  let wmsLayer = new ol.layer.Tile({
+  wmsLayer = new ol.layer.Tile({
     source: wmsSource
   });
-  let view = map.getView();
 
   let vectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector(),
@@ -38,121 +170,8 @@ let mapReady = (map) => {
 
   map.un('click', mapOnClicked);
 
-  const getFeature = (layerSource, coordinates, renderResult) => {
-    let _coordinates = ol.proj.transform(coordinates, 'EPSG:3857', 'EPSG:4326');
-    if (markerSource) {
-      markerSource.clear();
-    }
-    $('#latitude').val('');
-    $('#longitude').val('');
-
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        type: 'POST',
-        url: '/get_feature/',
-        data: {
-          'layerSource': layerSource
-        },
-        success: function (result) {
-          const data = result['feature_data'];
-          let objectData = {};
-          if (data.constructor === Object) {
-            objectData = data;
-          } else {
-            try {
-              objectData = JSON.parse(data);
-            } catch (e) {
-              console.log(e);
-              reject(e);
-              return;
-            }
-          }
-          let features = objectData['features'];
-          if (features.length > 0) {
-            const feature = features[0];
-            const bbox = feature['bbox'];
-
-            map.getView().fit(bbox, {size: map.getSize()});
-            if (renderResult) {
-              $('#latitude').val(_coordinates[1]);
-              $('#longitude').val(_coordinates[0]);
-
-              $('#fetch-river-name').attr('disabled', false);
-              $('#fetch-geomorphological-zone').attr('disabled', false);
-              $('#wetland-site-code').attr('disabled', false);
-
-              $('#site_code').val('');
-              $('#geomorphological_zone').val('');
-
-              let olFeature = new ol.format.GeoJSON().readFeatures(feature);
-
-              const format = new ol.format.GeoJSON();
-              const featureObj = format.readFeature(feature, {
-                dataProjection: 'EPSG:3857',
-                featureProjection: 'EPSG:3857'
-              })
-              const geometry = featureObj.getGeometry();
-              geometry.transform('EPSG:3857', 'EPSG:4326');
-
-              const convertedGeojsonStr = format.writeFeature(featureObj, {
-                dataProjection: 'EPSG:4326',
-                featureProjection: 'EPSG:4326'
-              });
-
-              $('#site-geometry').val(convertedGeojsonStr);
-              // vectorLayer.getSource().clear()
-              // vectorLayer.getSource().addFeatures(olFeature);
-              wetlandData = feature['properties'];
-              $('#additional-data').val(JSON.stringify(wetlandData));
-              $('#river_name').val(wetlandData['name'] ? wetlandData['name'] : '-');
-
-              moveMarkerOnTheMap(_coordinates[1], _coordinates[0], false);
-            }
-          }
-          resolve(features);
-        },
-        error: function (err) {
-          console.log(err);
-          reject(err);
-        }
-      });
-    });
-  };
-
-
   map.on('click', function(e) {
-    let layerSource = wmsLayer.getSource().getGetFeatureInfoUrl(
-      e.coordinate,
-      view.getResolution(),
-      view.getProjection(),
-      {'INFO_FORMAT': 'application/json'}
-    );
-    layerSource += '&QUERY_LAYERS=' + wmsLayerName;
-    const zoomLevel = map.getView().getZoom();
-    $('#map-loading').css('display', 'flex');
-    getFeature(layerSource, e.coordinate,zoomLevel > 10).then(function(features) {
-        if (features.length > 0 && zoomLevel <= 10) {
-          let bbox = features[0]['bbox'];
-          let centroidX = (bbox[0] + bbox[2]) / 2;
-          let centroidY = (bbox[1] + bbox[3]) / 2;
-          let point = [centroidX, centroidY];
-
-          layerSource = wmsLayer.getSource().getGetFeatureInfoUrl(
-            point,
-            view.getResolution(),
-            view.getProjection(),
-            {'INFO_FORMAT': 'application/json'}
-          );
-          layerSource += '&QUERY_LAYERS=' + wmsLayerName;
-          getFeature(layerSource, e.coordinate, true).then(function (f) {
-            $('#map-loading').hide();
-          })
-        } else {
-          $('#map-loading').hide();
-        }
-    }).catch(function(error) {
-        // handle any errors
-    });
+    mapClicked(e.coordinate);
   });
 }
 
