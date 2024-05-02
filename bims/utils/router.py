0201d20879f4f027f4fc474fcac6fc3e23f46b92
Dimas Ciputra <dimas.ciputra@gmail.com>
2024-05-02 14:00:59 +0100

    Fix db replica for multi tenant (#3868)

--- bims/utils/router.py
+++ bims/utils/router.py
@@ -1,7 +1,45 @@
 import random
 from django.conf import settings
+from django.db import connections
+from django_tenants.middleware import TenantMainMiddleware
+from django_tenants.utils import get_tenant_domain_model
 
 PRIMARY = "default"
+
+
+class ReadReplicaTenantMiddleware(TenantMainMiddleware):
+    def process_request(self, request):
+        # Connection needs first to be at the public schema, as this is where
+        # the tenant metadata is stored.
+
+        for con in connections:
+            connections[con].set_schema_to_public()
+        hostname = self.hostname_from_request(request)
+
+        domain_model = get_tenant_domain_model()
+        try:
+            tenant = self.get_tenant(domain_model, hostname)
+        except domain_model.DoesNotExist:
+            return self.no_tenant_found(request, hostname)
+
+        tenant.domain_url = hostname
+        request.tenant = tenant
+        for con in connections:
+            connections[con].set_tenant(request.tenant)
+        self.setup_url_routing(request)
+
+
+def extra_set_tenant_stuff(wrapper_class, tenant):
+    try:
+        tenant_replica_connection = connections["replica_1"]
+    except Exception:  # noqa
+        return
+    if (
+            wrapper_class.settings_dict["DATABASE"] == "default"
+            and tenant_replica_connection.schema_name != tenant.schema_name
+    ):
+        tenant_replica_connection.set_schema(tenant.schema_name)
+        print(f"changing replica schema to {tenant.schema_name}")
 
 
 class PrimaryReplicaRouter:
