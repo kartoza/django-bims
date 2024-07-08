17a7737a7937534c71492aa6c21bec0516eaf035
Dimas Ciputra <dimas.ciputra@gmail.com>
2024-07-08 10:33:38 +0100

    Implement pdf checklist (#4047)
    * Implement pdf checklist
    
    * Remove print

--- bims/api_views/checklist.py
+++ bims/api_views/checklist.py
@@ -2,14 +2,24 @@ import csv
 import os
 from django.conf import settings
 from django.http import Http404
+from django.template.loader import render_to_string
 from preferences import preferences
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import cm
+from reportlab.platypus import SimpleDocTemplate, TableStyle, Table, Spacer
+from reportlab.platypus.para import Paragraph
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from bims.api_views.search import CollectionSearch
 from bims.models.taxonomy import Taxonomy
 from bims.models.download_request import DownloadRequest
-from bims.serializers.checklist_serializer import ChecklistSerializer
+from bims.serializers.checklist_serializer import (
+    ChecklistSerializer,
+    ChecklistPDFSerializer
+)
 from bims.utils.url import parse_url_to_filters
 from bims.tasks.checklist import download_checklist
 from bims.tasks.email_csv import send_csv_via_email
@@ -61,10 +71,22 @@ def generate_checklist(download_request_id):
     batch_size = 1000
     collection_records = search.process_search().distinct('taxonomy')
 
+    if (
+        download_request.resource_type and
+        download_request.resource_type.lower() == 'pdf'
+    ):
+        return generate_pdf_checklist(
+            download_request, collection_records, batch_size)
+    else:
+        return generate_csv_checklist(
+            download_request, collection_records, batch_size)
+
+
+def generate_csv_checklist(download_request, collection_records, batch_size):
     csv_file_path = os.path.join(
         settings.MEDIA_ROOT,
         'checklists',
-        f'checklist_{download_request_id}.csv')
+        f'checklist_{download_request.id}.csv')
     os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
 
     fieldnames = [key for key in get_serializer_keys(ChecklistSerializer) if key != 'id']
@@ -83,9 +105,76 @@ def generate_checklist(download_request_id):
             batch = collection_records[start:start + batch_size]
             process_batch(batch, writer, written_taxa_ids)
 
-    # Save file path to download_request
     download_request.processing = False
     download_request.request_file = csv_file_path
+    download_request.save()
+
+    return True
+
+
+def generate_pdf_checklist(download_request, collection_records, batch_size):
+    pdf_file_path = os.path.join(
+        settings.MEDIA_ROOT,
+        'checklists',
+        f'checklist_{download_request.id}.pdf')
+    os.makedirs(os.path.dirname(pdf_file_path), exist_ok=True)
+
+    written_taxa_ids = set()
+    all_taxa = []
+
+    for start in range(0, collection_records.count(), batch_size):
+        batch = collection_records[start:start + batch_size]
+        record_taxonomy_ids = batch.values_list('taxonomy_id', flat=True)
+        unique_taxonomy_ids = set(record_taxonomy_ids) - written_taxa_ids
+
+        if unique_taxonomy_ids:
+            taxa = Taxonomy.objects.filter(id__in=unique_taxonomy_ids)
+            taxon_serializer = ChecklistPDFSerializer(taxa, many=True)
+            for taxon in taxon_serializer.data:
+                written_taxa_ids.add(taxon['id'])
+                del taxon['id']
+                all_taxa.append([
+                    Paragraph(taxon['scientific_name'], getSampleStyleSheet()['Normal']),
+                    Paragraph(taxon['common_name'], getSampleStyleSheet()['Normal']),
+                    Paragraph(taxon['threat_status'], getSampleStyleSheet()['Normal']),
+                    Paragraph(taxon['sources'], getSampleStyleSheet()['Normal'])
+                ])
+
+    # Generate the PDF
+    doc = SimpleDocTemplate(
+        pdf_file_path,
+        pagesize=landscape(A4), topMargin=0.5 * cm, bottomMargin=0.35 * cm)
+    elements = []
+
+    # Define table style
+    style_table = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), '#0D3511'),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 11),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 5),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('LEFTPADDING', (0, 0), (-1, -1), 2),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 2),
+        ('TOPPADDING', (0, 0), (-1, -1), 2),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 2),
+    ])
+
+    data = [['Scientific Name', 'Common Name', 'Threat Status', 'Sources']] + all_taxa
+    table_width = doc.width
+    col_widths = [table_width * 0.35, table_width * 0.30, table_width * 0.15, table_width * 0.30]
+
+    table = Table(data, colWidths=col_widths)
+    table.setStyle(style_table)
+
+    elements.append(table)
+    doc.build(elements)
+
+    download_request.processing = False
+    download_request.request_file = pdf_file_path
     download_request.save()
 
     return True
