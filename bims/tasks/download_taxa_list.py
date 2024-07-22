67c1966c9451b3d6538ca824628b98aea83a74ea
Dimas Ciputra <dimas.ciputra@gmail.com>
2024-07-22 16:01:39 +0100

    Improve taxa download (#4093)

--- bims/tasks/download_taxa_list.py
+++ bims/tasks/download_taxa_list.py
@@ -4,36 +4,29 @@ import logging
 from django.contrib.auth import get_user_model
 from celery import shared_task
 
+
 logger = logging.getLogger(__name__)
 
 
 def process_download_csv_taxa_list(request, csv_file_path, filename, user_id, download_request_id=''):
-    from bims.views.download_csv_taxa_list import TaxaCSVSerializer
-    from bims.tasks.email_csv import send_csv_via_email
     from bims.api_views.taxon import TaxaList
+    from bims.views.download_csv_taxa_list import TaxaCSVSerializer
+    from bims.tasks import send_csv_via_email
 
     class RequestGet:
         def __init__(self, get_data):
             self.GET = get_data
+
+    # Prepare the request object
     request_get = RequestGet(request)
+
+    # Get the taxa list based on request parameters
     taxa_list = TaxaList.get_taxa_by_parameters(request_get)
 
-    taxa_serializer = TaxaCSVSerializer(
-        taxa_list,
-        many=True
-    )
-
-    rows = taxa_serializer.data
-    headers = taxa_serializer.context['headers']
-
-    with open(csv_file_path, 'w') as csv_file:
-        writer = csv.writer(
-            csv_file, delimiter=',', quotechar='"',
-            quoting=csv.QUOTE_MINIMAL)
-
-        updated_headers = []
-
-        for header in headers:
+    # Define the header update function
+    def update_headers(_headers):
+        _updated_headers = []
+        for header in _headers:
             if header == 'class_name':
                 header = 'class'
             elif header == 'taxon_rank':
@@ -45,12 +38,27 @@ def process_download_csv_taxa_list(request, csv_file_path, filename, user_id, do
                 header = header.replace('_', ' ').capitalize()
             if header == 'Sub species':
                 header = 'SubSpecies'
-            updated_headers.append(header)
+            _updated_headers.append(header)
+        return _updated_headers
+
+    # Serialize a single item to extract headers
+    sample_item = next(taxa_list.iterator())
+    sample_serializer = TaxaCSVSerializer(sample_item)
+    headers = list(sample_serializer.data.keys())
+    updated_headers = update_headers(headers)
+
+    # Write data to CSV
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(updated_headers)
 
-        for taxon_row in rows:
-            writer.writerow([value for key, value in taxon_row.items()])
+        # Process each item in the queryset individually
+        for taxon in taxa_list.iterator():
+            serializer = TaxaCSVSerializer(taxon)
+            row = serializer.data
+            writer.writerow([value for key, value in row.items()])
 
+    # Send the CSV file via email
     UserModel = get_user_model()
     try:
         user = UserModel.objects.get(id=user_id)
