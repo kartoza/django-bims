9e54ad25aeaa85dfc1464293a1583a432117cf65
Dimas <dimas@kartoza.com>
2024-08-05 13:30:00 +0100

    Ignore easy-audit pre-save error

--- bims/utils/sentry.py
+++ bims/utils/sentry.py
@@ -12,6 +12,10 @@ def before_send(event, hint):
         logging.info(f"Exception type: {exc_type}")
         logging.info(f"Exception value: {exc_value}")
 
+        if exc_value and str(exc_value) == "easy audit had a pre-save exception.":
+            logging.info("Ignoring 'easy audit had a pre-save exception.' error")
+            return None
+
         if 'easyaudit.signals.model_signals' in str(exc_value):
             logging.info("Ignoring easyaudit.signals.model_signals error")
             return None
