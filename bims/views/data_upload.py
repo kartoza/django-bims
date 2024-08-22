ac97bef068ed68b66d3bf77539f5f620d5bdb1d2
Dimas <dimas@kartoza.com>
2024-08-22 16:45:54 +0100

    Convert xls to csv in uploader

--- bims/views/data_upload.py
+++ bims/views/data_upload.py
@@ -3,8 +3,12 @@
 """
 
 import ast
+import os
 from datetime import datetime
 
+import pandas as pd
+from django.core.files.base import ContentFile
+from django.core.files.temp import NamedTemporaryFile
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.views.generic import TemplateView
@@ -52,7 +56,7 @@ class DataUploadView(
         return context
 
     def post(self, request, *args, **kwargs):
-        csv_file = request.FILES.get('csv_file')
+        upload_file = request.FILES.get('csv_file')
         taxon_group_id = request.POST.get('taxon_group', None)
         taxon_group_logo = request.FILES.get('taxon_group_logo')
         taxon_group_name = request.POST.get('taxon_group_name', '')
@@ -84,15 +88,42 @@ class DataUploadView(
                 display_order=display_order
             )
             return HttpResponseRedirect(request.path_info)
-        if not csv_file:
-            raise Http404('Missing csv file')
-        upload_session = UploadSession.objects.create(
-            uploader=request.user,
-            process_file=csv_file,
-            uploaded_at=datetime.now(),
-            module_group_id=taxon_group_id,
-            category=self.category
-        )
+        if not upload_file:
+            raise Http404('Missing file')
+
+        file_extension = os.path.splitext(upload_file.name)[1].lower()
+
+        try:
+            if file_extension in ['.xls', '.xlsx']:
+                excel_data = pd.read_excel(upload_file)
+                with NamedTemporaryFile(delete=False, suffix=".csv") as temp_csv_file:
+                    csv_file_path = temp_csv_file.name
+                    excel_data.to_csv(csv_file_path, index=False)
+
+                    with open(csv_file_path, 'rb') as csv_file:
+                        csv_content = ContentFile(csv_file.read())
+                    csv_file_name = f"{os.path.splitext(upload_file.name)[0]}.csv"
+                    upload_session = UploadSession.objects.create(
+                        uploader=request.user,
+                        uploaded_at=datetime.now(),
+                        module_group_id=taxon_group_id,
+                        category=self.category
+                    )
+                    upload_session.process_file.save(csv_file_name, csv_content)
+            elif file_extension == '.csv':
+                upload_session = UploadSession.objects.create(
+                    uploader=request.user,
+                    process_file=upload_file,
+                    uploaded_at=datetime.now(),
+                    module_group_id=taxon_group_id,
+                    category=self.category
+                )
+
+            else:
+                raise Http404('Unsupported file type')
+
+        except Exception as e:
+            raise Http404(f'Failed to process file: {str(e)}')
         if self.upload_task:
             self.upload_task.delay(upload_session.id)
         return HttpResponseRedirect(request.path_info)
