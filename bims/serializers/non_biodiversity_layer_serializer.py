ade3eecac0d7da76f8c234599cfd4a8ebc4e8506
Dimas Ciputra <dimas.ciputra@gmail.com>
2024-10-10 16:19:42 +0100

    Add pmtiles support (#4303)

--- bims/serializers/non_biodiversity_layer_serializer.py
+++ bims/serializers/non_biodiversity_layer_serializer.py
@@ -8,10 +8,27 @@ class NonBiodiversityLayerSerializer(serializers.ModelSerializer):
     """
     native_layer_url = serializers.SerializerMethodField()
     native_layer_style = serializers.SerializerMethodField()
+    pmtiles = serializers.SerializerMethodField()
+    attribution = serializers.SerializerMethodField()
+
+    def get_pmtiles(self, obj: NonBiodiversityLayer):
+        request = self.context.get('request', None)
+        if not obj.native_layer:
+            return ''
+        if not obj.native_layer.pmtile:
+            return ''
+        return (
+            obj.native_layer.absolute_pmtiles_url(request)
+        ).replace('pmtiles://', '')
 
     def get_native_layer_url(self, obj: NonBiodiversityLayer) -> str:
         if obj.native_layer:
             return obj.native_layer.tile_url
+        return ''
+
+    def get_attribution(self, obj: NonBiodiversityLayer) -> str:
+        if obj.native_layer and obj.native_layer.attribution:
+            return obj.native_layer.attribution
         return ''
 
     def get_native_layer_style(self, obj: NonBiodiversityLayer) -> str:
