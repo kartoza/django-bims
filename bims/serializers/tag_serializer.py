937e04ac237f9aaae7e17ccd5c32f3be2e034070
Dimas Ciputra <dimas.ciputra@gmail.com>
2026-02-05 08:13:42 +0000

    Add tag description, and display it in the frontend (#5042)

--- bims/serializers/tag_serializer.py
+++ bims/serializers/tag_serializer.py
@@ -2,19 +2,29 @@ from rest_framework import serializers
 from taggit.models import Tag
 
 from bims.models.taxonomy import Taxonomy, TaxonTag
+from bims.models.taxon_tag_description import TaxonTagDescription
 
 
 class TagSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
     def get_name(self, obj):
         if isinstance(obj, TaxonTag):
             return f'{obj.name} {"(?)" if obj.doubtful else ""}'
         return obj.name
 
+    def get_description(self, obj):
+        try:
+            return obj.description_detail.description
+        except TaxonTagDescription.DoesNotExist:
+            return ''
+        except AttributeError:
+            return ''
+
     class Meta:
         model = Tag
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'description']
 
 
 class TaxonomyTagUpdateSerializer(serializers.ModelSerializer):
