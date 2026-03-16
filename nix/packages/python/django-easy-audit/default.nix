e0d216eae2f8f9ee132b5a3fdc20648da5c69df5
Tim Sutton <tim@kartoza.com>
2026-03-16 23:49:41 +0000

    Fix django-easy-audit to use pyproject format

--- nix/packages/python/django-easy-audit/default.nix
+++ nix/packages/python/django-easy-audit/default.nix
@@ -2,19 +2,22 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   django,
 }:
 
 buildPythonPackage rec {
   pname = "django-easy-audit";
   version = "1.3.8";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "django_easy_audit";
     inherit version;
     hash = "sha256-XR8qANW6qG19LcliwdEdOkGq88iUPbsDrMG0cVNx1qc=";
   };
+
+  build-system = [ setuptools ];
 
   dependencies = [ django ];
 
