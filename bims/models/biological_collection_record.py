ece4a8266818191803641ca7940d79da12a45530
Dimas Ciputra <dimas.ciputra@gmail.com>
2026-04-08 11:30:24 +0100

    Store uncertainty data from GBIF in collection record table (#5212)

--- bims/models/biological_collection_record.py
+++ bims/models/biological_collection_record.py
@@ -407,6 +407,26 @@ class BiologicalCollectionRecord(AbstractValidation):
         verbose_name='Identifier',
     )
 
+    coordinate_precision = models.DecimalField(
+        max_digits=10,
+        decimal_places=6,
+        blank=True,
+        null=True,
+        help_text='A decimal representation of the precision of the coordinates. '
+                  'Examples: 0.00001 (GPS limit), 0.000278 (nearest second), '
+                  '0.01667 (nearest minute), 1.0 (nearest degree)'
+    )
+
+    coordinate_uncertainty_in_meters = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        help_text='The horizontal distance (in meters) from the given coordinates '
+                  'describing the smallest circle containing the whole of the Location. '
+                  'Examples: 30 (GPS after 2000-05-01), 100 (GPS before 2000-05-01)'
+    )
+
     licence = models.ForeignKey(
         'bims.Licence',
         null=True,
