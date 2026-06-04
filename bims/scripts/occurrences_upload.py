13052a6a9b7bfbe1465e0132fb001616ce2d4b02
Dimas <dimas.ciputra@gmail.com>
2026-06-04 11:13:08 +0100

    Fix and improve site code generator for FBIS

--- bims/scripts/occurrences_upload.py
+++ bims/scripts/occurrences_upload.py
@@ -586,20 +586,23 @@ class OccurrenceProcessor(object):
                     )
 
         if not location_site.site_code:
-            site_code, catchments_data = generate_site_code(
-                location_site,
-                lat=location_site.latitude,
-                lon=location_site.longitude,
-                river_name=river_name,
-                ecosystem_type=location_site.ecosystem_type,
-                wetland_name=user_wetland_name,
-                **{
-                    "site_desc": site_description,
-                    "site_name": location_site_name
-                }
-            )
-            location_site.site_code = site_code
-            self._log(logging.DEBUG, f"Generated site_code={site_code}")
+            try:
+                site_code, catchments_data = generate_site_code(
+                    location_site,
+                    lat=location_site.latitude,
+                    lon=location_site.longitude,
+                    river_name=river_name,
+                    ecosystem_type=location_site.ecosystem_type or '',
+                    wetland_name=user_wetland_name or '',
+                    **{
+                        "site_desc": site_description,
+                        "site_name": location_site_name
+                    }
+                )
+                location_site.site_code = site_code
+                self._log(logging.DEBUG, f"Generated site_code={site_code}")
+            except Exception as e:
+                self._log(logging.WARNING, f"Site code generation failed for site id={location_site.id}: {e}")
         if accuracy_of_coordinates:
             location_site.accuracy_of_coordinates = accuracy_of_coordinates
         location_site.save()
--- bims/utils/site_code.py
+++ bims/utils/site_code.py
@@ -16,6 +16,7 @@ from requests.exceptions import HTTPError
 from bims.utils.get_key import get_key
 from cloud_native_gis.models import Layer
 from cloud_native_gis.utils.geometry import query_features
+from bims.utils.geometry import get_feature_data as get_river_feature_data
 from sass.models import River
 
 FBIS_CATCHMENT_KEY = 'river_catchment_areas_group'
@@ -244,22 +245,22 @@ def fbis_catchment_generator(
         elif location_site.river:
             river_name = location_site.river.name
         else:
-            river_name = get_feature_data(
+            river_name = get_river_feature_data(
                 lon=location_site.geometry_point[0],
                 lat=location_site.geometry_point[1],
-                context_key='river_name',
-                layer_name='river',
-                tolerance=0.5
+                context_key='name',
+                layer_name='SA_RIVERS',
+                tolerance=1000
             )
 
     # Search river name by coordinates
     if not river_name and lat and lon:
-        river_name = get_feature_data(
+        river_name = get_river_feature_data(
             lon=lon,
             lat=lat,
-            context_key='river_name',
-            layer_name='river',
-            tolerance=0.5
+            context_key='name',
+            layer_name='SA_RIVERS',
+            tolerance=1000
         )
 
     if not river_name:
